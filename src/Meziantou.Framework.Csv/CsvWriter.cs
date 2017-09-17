6b6df742b1905de299bb62271ce58b1b57e2df5a
meziantou <meziantou@users.noreply.github.com>
2017-09-17 12:13:58 -0400

    Add CsvWriter

---
+++ src/Meziantou.Framework.Csv/CsvWriter.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meziantou.Framework.Csv
+{
+    public class CsvWriter
+    {
+        private bool _isFirstRow = true;
+        private bool _isFirstRowValue = true;
+
+        public TextWriter BaseWriter { get; }
+        public string EndOfLine { get; set; } = Environment.NewLine;
+        public char Separator { get; set; } = CsvReader.DefaultSeparatorCharacter;
+        public char? Quote { get; set; } = CsvReader.DefaultQuoteCharacter;
+
+        public CsvWriter(TextWriter textReader)
+        {
+            BaseWriter = textReader ?? throw new ArgumentNullException(nameof(textReader));
+        }
+
+        public Task BeginRowAsync()
+        {
+            var isFirstRow = _isFirstRow;
+            _isFirstRow = false;
+            _isFirstRowValue = true;
+            if (!isFirstRow)
+            {
+                return BaseWriter.WriteAsync(EndOfLine);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task WriteValueAsync(string value)
+        {
+            var sb = new StringBuilder();
+            if (!_isFirstRowValue)
+            {
+                sb.Append(Separator);
+            }
+
+            _isFirstRowValue = false;
+            if (!string.IsNullOrEmpty(value))
+            {
+                if (Quote.HasValue)
+                {
+                    var quote = Quote.Value;
+                    if (value[0] != quote && value.IndexOf(Separator) < 0)
+                    {
+                        sb.Append(value);
+                    }
+                    else
+                    {
+                        sb.Append(quote);
+                        for (var i = 0; i < value.Length; i++)
+                        {
+                            var c = value[i];
+                            if (c == quote)
+                            {
+                                sb.Append(quote);
+                                sb.Append(quote);
+                            }
+                            else
+                            {
+                                sb.Append(c);
+                            }
+                        }
+
+                        sb.Append(Quote);
+                    }
+                }
+                else
+                {
+                    sb.Append(value);
+                }
+            }
+
+            return BaseWriter.WriteAsync(sb.ToString());
+        }
+
+        public async Task WriteValuesAsync(IEnumerable<string> values)
+        {
+            foreach (var value in values)
+            {
+                await WriteValueAsync(value).ConfigureAwait(false);
+            }
+        }
+
+        public Task WriteValuesAsync(params string[] values)
+        {
+            return WriteValuesAsync((IEnumerable<string>)values);
+        }
+
+        public Task WriteRowAsync(params string[] values)
+        {
+            return WriteRowAsync((IEnumerable<string>)values);
+        }
+
+        public async Task WriteRowAsync(IEnumerable<string> values)
+        {
+            await BeginRowAsync().ConfigureAwait(false);
+            await WriteValuesAsync(values).ConfigureAwait(false);
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.Csv.Tests/CsvWriterTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meziantou.Framework.Csv.Tests
+{
+    [TestClass]
+    public class CsvWriterTests
+    {
+        [TestMethod]
+        public async Task CsvWriter_NoEscape()
+        {
+            using (var sw = new StringWriter())
+            {
+                var writer = new CsvWriter(sw);
+                await writer.WriteRowAsync("A", "B");
+                await writer.WriteRowAsync("C", "D");
+
+                Assert.AreEqual(@"A,B
+C,D", sw.ToString());
+            }
+        }
+
+        [TestMethod]
+        public async Task CsvWriter_EscapeValueWithSeparator()
+        {
+            using (var sw = new StringWriter())
+            {
+                var writer = new CsvWriter(sw);
+                await writer.WriteRowAsync("A", "B,");
+                await writer.WriteRowAsync("C", "D");
+
+                Assert.AreEqual(@"A,""B,""
+C,D", sw.ToString());
+            }
+        }
+
+        [TestMethod]
+        public async Task CsvWriter_EscapeValueWithStartingQuote()
+        {
+            using (var sw = new StringWriter())
+            {
+                var writer = new CsvWriter(sw);
+                await writer.WriteRowAsync("A", "\"B");
+
+                Assert.AreEqual("A,\"\"\"B\"", sw.ToString());
+            }
+        }
+
+        [TestMethod]
+        [DataRow("A;B:D;E")]
+        [DataRow("A,;B:D;E")]
+        [DataRow(",A;B:D;E")]
+        [DataRow("A;\"B:D;E")]
+        [DataRow("A;B\":D;E")]
+        public async Task CsvWriter_CsvReader(string data)
+        {
+            var rows = new List<List<string>>();
+            foreach (var row in data.Split(':'))
+            {
+                rows.Add(new List<string>(row.Split(';')));
+            }
+
+            using (var sw = new StringWriter())
+            {
+                var writer = new CsvWriter(sw);
+                foreach (var row in rows)
+                {
+                    await writer.WriteRowAsync(row);
+                }
+
+                var csv = sw.ToString();
+                using (var sr = new StringReader(csv))
+                {
+                    var reader = new CsvReader(sr);
+
+                    var rowIndex = -1;
+                    CsvRow csvRow;
+                    while ((csvRow = await reader.ReadRowAsync()) != null)
+                    {
+                        rowIndex++;
+                        CollectionAssert.AreEqual(rows[rowIndex], csvRow.Values.ToList());
+                    }
+
+                    Assert.AreEqual(rows.Count - 1, rowIndex);
+                }
+            }
+        }
+
+    }
+}

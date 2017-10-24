c02a5b97c5ddf8d637e04e6d84dabfb93d63b352
meziantou <meziantou@users.noreply.github.com>
2017-10-23 23:03:48 -0400

    Add ReflectionDynamicObject

---
+++ src/Meziantou.Framework/Utilities/ReflectionDynamicObject.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+
+namespace Meziantou.Framework.Utilities
+{
+    public class ReflectionDynamicObject : DynamicObject
+    {
+        private const BindingFlags DefaultBindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+        private static Dictionary<Type, TypeCache> _cache = new Dictionary<Type, TypeCache>();
+
+        private readonly object _originalObject;
+        private TypeCache _typeCache;
+
+        public ReflectionDynamicObject(object obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+
+            _originalObject = obj;
+            var type = obj.GetType();
+            if (!_cache.TryGetValue(type, out var typeCache))
+            {
+                typeCache = TypeCache.Create(type);
+                _cache[type] = typeCache;
+            }
+
+            _typeCache = typeCache;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return TryGetMemberValue(binder.Name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            return TrySetMemberValue(binder.Name, value);
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            foreach (var indexer in _typeCache.Indexers)
+            {
+                try
+                {
+                    result = indexer.GetValue(_originalObject, indexes);
+                    return true;
+                }
+                catch (ArgumentException)
+                {
+                }
+                catch (TargetParameterCountException)
+                {
+                }
+                catch (TargetInvocationException)
+                {
+                }
+            }
+
+            result = null;
+            return false;
+        }
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            foreach (var indexer in _typeCache.Indexers)
+            {
+                try
+                {
+                    indexer.SetValue(_originalObject, value, indexes);
+                    return true;
+                }
+                catch (ArgumentException)
+                {
+                }
+                catch (TargetParameterCountException)
+                {
+                }
+                catch (TargetInvocationException)
+                {
+                }
+            }
+
+            return false;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            var type = _originalObject.GetType();
+            while (type != null)
+            {
+                try
+                {
+                    result = type.InvokeMember(binder.Name, DefaultBindingFlags | BindingFlags.InvokeMethod, null, _originalObject, args);
+                    return true;
+                }
+                catch (MissingMethodException)
+                {
+                    type = type.BaseType;
+                }
+            }
+
+            result = null;
+            return false;
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            foreach (var propertyInfo in _typeCache.Properties)
+            {
+                yield return propertyInfo.Key;
+            }
+
+            foreach (var field in _typeCache.Indexers)
+            {
+                yield return field.Name;
+            }
+
+            foreach (var field in _typeCache.Fields)
+            {
+                yield return field.Key;
+            }
+        }
+
+        public override bool TryConvert(ConvertBinder binder, out object result)
+        {
+            result = Convert.ChangeType(_originalObject, binder.Type);
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return _originalObject.ToString();
+        }
+
+        private bool TryGetMemberValue(string name, out object result)
+        {
+            if (_typeCache.Properties.TryGetValue(name, out PropertyInfo property))
+            {
+                result = property.GetValue(_originalObject);
+                return true;
+            }
+
+            if (_typeCache.Fields.TryGetValue(name, out FieldInfo field))
+            {
+                result = field.GetValue(_originalObject);
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+        private bool TrySetMemberValue(string name, object value)
+        {
+            if (_typeCache.Properties.TryGetValue(name, out PropertyInfo property))
+            {
+                property.SetValue(_originalObject, value);
+                return true;
+            }
+
+            if (_typeCache.Fields.TryGetValue(name, out FieldInfo field))
+            {
+                field.SetValue(_originalObject, value);
+                return true;
+            }
+
+            return false;
+        }
+
+        private class TypeCache
+        {
+            private TypeCache()
+            {
+            }
+
+            public Dictionary<string, PropertyInfo> Properties { get; } = new Dictionary<string, PropertyInfo>();
+            public Dictionary<string, FieldInfo> Fields { get; } = new Dictionary<string, FieldInfo>();
+            public List<PropertyInfo> Indexers { get; } = new List<PropertyInfo>();
+
+            public static TypeCache Create(Type type)
+            {
+                var instance = new TypeCache();
+                while (type != null)
+                {
+                    foreach (var propertyInfo in type.GetProperties(DefaultBindingFlags))
+                    {
+                        if (propertyInfo.GetIndexParameters().Any())
+                        {
+                            instance.Indexers.Add(propertyInfo);
+                        }
+                        else
+                        {
+                            if (!instance.Properties.ContainsKey(propertyInfo.Name))
+                            {
+                                instance.Properties.Add(propertyInfo.Name, propertyInfo);
+                            }
+                        }
+                    }
+
+                    foreach (var fieldInfo in type.GetFields(DefaultBindingFlags))
+                    {
+                        if (!instance.Fields.ContainsKey(fieldInfo.Name))
+                        {
+                            instance.Fields.Add(fieldInfo.Name, fieldInfo);
+                        }
+                    }
+
+                    type = type.BaseType;
+                }
+
+                return instance;
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/Utilities/ReflectionDynamicObjectTests.cs
@@ -0,0 +1,98 @@
+﻿using Meziantou.Framework.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Meziantou.Framework.Tests.Utilities
+{
+    [TestClass]
+    public class ReflectionDynamicObjectTests
+    {
+        [TestMethod]
+        public void ReflectionDynamicObject()
+        {
+            var test = new Test();
+            dynamic rdo = new ReflectionDynamicObject(test);
+
+            Assert.AreEqual(42, rdo._privateField);
+            Assert.AreEqual(10, rdo.PrivateProperty);
+            Assert.AreEqual(1, rdo[1]);
+            Assert.AreEqual(3, rdo[1, 2]);
+            Assert.AreEqual("test", rdo["test"]);
+            rdo["test"] = "sample";
+            Assert.AreEqual("testsample", rdo["test"]);
+            Assert.AreEqual(42, rdo.PrivateMethod());
+            Assert.AreEqual(1, rdo.PrivateMethodWithOverload());
+            Assert.AreEqual(2, rdo.PrivateMethodWithOverload(1));
+            Assert.AreEqual(3L, rdo.PrivateMethodWithOverload(1L));
+            Assert.AreEqual("test", rdo.ProtectedVirtualMethod());
+
+            rdo._privateField = 43;
+            Assert.AreEqual(43, rdo._privateField);
+            Assert.AreEqual(43, rdo.PrivateField);
+        }
+
+        [TestMethod]
+        public void ReflectionDynamicObject_Inheritance()
+        {
+            var test = new Test2();
+            dynamic rdo = new ReflectionDynamicObject(test);
+
+            Assert.AreEqual(42, rdo._privateField);
+            Assert.AreEqual(11, rdo.PrivateProperty);
+            Assert.AreEqual("test2", rdo.ProtectedVirtualMethod());
+        }
+
+        private class Test
+        {
+            private int _privateField = 42;
+            public int PrivateField { get => _privateField; set => _privateField = value; }
+
+            private int PrivateProperty { get; set; } = 10;
+
+            private int PrivateMethod()
+            {
+                return _privateField;
+            }
+
+            private int PrivateMethodWithOverload()
+            {
+                return 1;
+            }
+
+            private int PrivateMethodWithOverload(int a)
+            {
+                return 1 + a;
+            }
+
+            private long PrivateMethodWithOverload(long a)
+            {
+                return 2 + a;
+            }
+
+            protected virtual string ProtectedVirtualMethod()
+            {
+                return "test";
+            }
+
+            private int this[int i] => i;
+            private int this[int i, int j] => i + j;
+
+
+            private string _indexer;
+            private string this[string str]
+            {
+                get => str + _indexer;
+                set => _indexer = value;
+            }
+        }
+
+        private class Test2 : Test
+        {
+            private int PrivateProperty { get; set; } = 11;
+
+            protected override string ProtectedVirtualMethod()
+            {
+                return "test2";
+            }
+        }
+    }
+}

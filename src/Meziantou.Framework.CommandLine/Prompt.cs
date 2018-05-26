d705d8f14c5b4c01c3f953b24fe72fa9babd6cba
Meziantou <meziantou@users.noreply.github.com>
2018-05-26 11:48:16 -0400

    Add Prompt method

---
+++ src/Meziantou.Framework.CommandLine/Prompt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Meziantou.Framework
+{
+    public static class Prompt
+    {
+        public static bool YesNo(string question, bool? defaultValue)
+        {
+            if (defaultValue.HasValue)
+            {
+                if (defaultValue.Value)
+                {
+                    return YesNo(question, "Y", "n", true);
+                }
+                else
+                {
+                    return YesNo(question, "y", "N", false);
+                }
+            }
+            else
+            {
+                return YesNo(question, "y", "n", null);
+            }
+        }
+
+        public static bool YesNo(string question, string yesValue, string noValue, bool? defaultValue)
+        {
+            while (true)
+            {
+                Console.Write($"{question} [{yesValue}/{noValue}] ");
+                var result = Console.ReadLine();
+                if (string.IsNullOrEmpty(result))
+                {
+                    if (defaultValue.HasValue)
+                        return defaultValue.Value;
+
+                    continue;
+                }
+
+                if (string.Equals(result, yesValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+                if (string.Equals(result, noValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}

a149a3c142a9dff559af1528839df580f04f3fac
Meziantou <meziantou@users.noreply.github.com>
2018-10-14 23:51:30 -0400

    Add FileLength

---
+++ src/Meziantou.Framework/FileLength.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Meziantou.Framework
+{
+    public readonly struct FileLength : IEquatable<FileLength>, IFormattable
+    {
+        public FileLength(long length)
+        {
+            Length = length;
+        }
+
+        public FileLength(string filePath)
+        {
+            Length = new FileInfo(filePath).Length;
+        }
+
+        public long Length { get; }
+
+        public override bool Equals(object obj) => obj is FileLength fileLength && Equals(fileLength);
+
+        public bool Equals(FileLength other) => Length == other.Length;
+
+        public override int GetHashCode() => Length.GetHashCode();
+
+        public override string ToString() => ToString(null, null);
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+                return Length.ToString(formatProvider);
+
+            var index = -1;
+            for (var i = 0; i < format.Length; i++)
+            {
+                char c = format[i];
+                if (c > '0' && c < '9')
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            var unitString = format;
+            if (index >= 0)
+            {
+                unitString = format.Substring(0, index);
+            }
+
+            if (!TryParseUnit(unitString, out var unit))
+                throw new ArgumentException("format is invalid", nameof(format));
+
+            var numberFormat = "G";
+            if (index > 0)
+            {
+                if (!int.TryParse(format.Substring(index), out var number))
+                    throw new ArgumentException("format is invalid", nameof(format));
+
+                numberFormat = "F" + number.ToString(CultureInfo.InvariantCulture);
+            }
+
+            return GetLength(unit).ToString(numberFormat);
+        }
+
+        public double GetLength(FileLengthUnit unit)
+        {
+            return (double)Length / (long)unit;
+        }
+
+        public static bool operator ==(FileLength length1, FileLength length2) => length1.Equals(length2);
+
+        public static bool operator !=(FileLength length1, FileLength length2) => !(length1 == length2);
+
+        public static implicit operator long(FileLength fileLength) => fileLength.Length;
+
+        public static implicit operator FileLength(long fileLength) => new FileLength(fileLength);
+
+        private static bool TryParseUnit(string unit, out FileLengthUnit result)
+        {
+            var last = unit[unit.Length - 1];
+            if (last != 'b' && last != 'B')
+            {
+                result = default;
+                return false;
+            }
+
+            if (unit.Length > 1)
+            {
+                var multiple = unit.Substring(0, unit.Length - 1);
+                switch (multiple.ToUpperInvariant())
+                {
+                    case "K":
+                        result = FileLengthUnit.KiloByte;
+                        return true;
+
+                    case "M":
+                        result = FileLengthUnit.MegaByte;
+                        return true;
+
+                    case "G":
+                        result = FileLengthUnit.GigaByte;
+                        return true;
+
+                    case "T":
+                        result = FileLengthUnit.TeraByte;
+                        return true;
+
+                    case "P":
+                        result = FileLengthUnit.PetaByte;
+                        return true;
+
+                    case "KI":
+                        result = FileLengthUnit.KibiByte;
+                        return true;
+
+                    case "MI":
+                        result = FileLengthUnit.MebiByte;
+                        return true;
+
+                    case "GI":
+                        result = FileLengthUnit.GibiByte;
+                        return true;
+
+                    case "TI":
+                        result = FileLengthUnit.TebiByte;
+                        return true;
+
+                    case "PI":
+                        result = FileLengthUnit.PebiByte;
+                        return true;
+                }
+            }
+
+            result = FileLengthUnit.Byte;
+            return true;
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/FileLengthTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Meziantou.Framework.Tests
+{
+    [TestClass]
+    public class FileLengthTests
+    {
+        [DataTestMethod]
+        [DataRow(10, "B", "10")]
+        [DataRow(1000, "kB", "1")]
+        [DataRow(1500, "kB", "1.5")]
+        [DataRow(1500, "kB2", "1.50")]
+        [DataRow(1024, "kiB", "1")]
+        [DataRow(1000000, "MB", "1")]
+        public void ToString_Test(long length, string format, string expectedValue)
+        {
+            var fileLength = new FileLength(length);
+            var actual = fileLength.ToString(format, CultureInfo.InvariantCulture);
+
+            Assert.AreEqual(expectedValue, actual);
+        }
+    }
+}

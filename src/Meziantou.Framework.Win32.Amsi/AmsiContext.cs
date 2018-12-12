43ab1f9dbf762cdb3ad11f29561807201b4e4a78
Meziantou <meziantou@users.noreply.github.com>
2018-12-11 22:09:30 -0500

    Rename AmsiApplication to AmsiContext

--- src/Meziantou.Framework.Win32.Amsi/AmsiContext.cs
+++ src/Meziantou.Framework.Win32.Amsi/AmsiContext.cs
@@ -3,22 +3,22 @@ using System.ComponentModel;
 
 namespace Meziantou.Framework.Win32
 {
-    public class AmsiApplication : IDisposable
+    public class AmsiContext : IDisposable
     {
         private readonly AmsiContextSafeHandle _context;
 
-        private AmsiApplication(AmsiContextSafeHandle context)
+        private AmsiContext(AmsiContextSafeHandle context)
         {
             _context = context;
         }
 
-        public static AmsiApplication Create(string applicationName)
+        public static AmsiContext Create(string applicationName)
         {
             int result = Amsi.AmsiInitialize(applicationName, out var context);
             if (result != 0)
                 throw new Win32Exception(result);
 
-            return new AmsiApplication(context);
+            return new AmsiContext(context);
         }
 
         public AmsiSession CreateSession()
--- tests/Meziantou.Framework.Win32.AmsiTests/AmsiContextTests.cs
+++ tests/Meziantou.Framework.Win32.AmsiTests/AmsiContextTests.cs
@@ -6,7 +6,7 @@ namespace Meziantou.Framework.Win32.AmsiTests
 {
     [TestClass]
     [DoNotParallelize]
-    public class AmsiApplicationTests
+    public class AmsiContextTests
     {
         [ClassInitialize]
         public static void ClassInitialize(TestContext _)
@@ -20,7 +20,7 @@ namespace Meziantou.Framework.Win32.AmsiTests
         [TestMethod]
         public void AmsiShouldDetectMalware_Buffer()
         {
-            using (var application = AmsiApplication.Create("MyApplication"))
+            using (var application = AmsiContext.Create("MyApplication"))
             using (var session = application.CreateSession())
             {
                 // https://en.wikipedia.org/wiki/EICAR_test_file
@@ -32,7 +32,7 @@ namespace Meziantou.Framework.Win32.AmsiTests
         [TestMethod]
         public void AmsiShouldDetectMalware_String()
         {
-            using (var application = AmsiApplication.Create("MyApplication"))
+            using (var application = AmsiContext.Create("MyApplication"))
             using (var session = application.CreateSession())
             {
                 // https://en.wikipedia.org/wiki/EICAR_test_file

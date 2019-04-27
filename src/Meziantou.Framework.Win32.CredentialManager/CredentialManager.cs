109d9fef5f9c84d1d975f2a1bc61d84b0e00e82e
Gérald Barré <meziantou@users.noreply.github.com>
2019-04-26 22:57:34 -0400

    Build samples in CI (#18)

--- src/Meziantou.Framework.Win32.CredentialManager/CredentialManager.cs
+++ src/Meziantou.Framework.Win32.CredentialManager/CredentialManager.cs
@@ -7,22 +7,6 @@ using Meziantou.Framework.Win32.Natives;
 
 namespace Meziantou.Framework.Win32
 {
-    public class CredentialResult
-    {
-        public CredentialResult(string userName, string password, string domain, CredentialSaveOption credentialSaved)
-        {
-            UserName = userName;
-            Password = password;
-            Domain = domain;
-            CredentialSaved = credentialSaved;
-        }
-
-        public string UserName { get; }
-        public string Password { get; }
-        public string Domain { get; }
-        public CredentialSaveOption CredentialSaved { get; }
-    }
-
     public static class CredentialManager
     {
         public static Credential ReadCredential(string applicationName)

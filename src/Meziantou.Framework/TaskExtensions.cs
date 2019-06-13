f5932ccaf174f8c73858fbfdf1af7436523516ac
meziantou <meziantou@users.noreply.github.com>
2019-06-13 13:24:33 -0400

    Add Task.Forget

---
+++ src/Meziantou.Framework/TaskExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace Meziantou.Framework
+{
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task)
+        {
+            // Only care about tasks that may fault or are faulted,
+            // so fast-path for SuccessfullyCompleted and Canceled tasks
+            if (!task.IsCompleted || task.IsFaulted)
+            {
+                _ = ForgetAwaited(task);
+            }
+
+            async static Task ForgetAwaited(Task task)
+            {
+                try
+                {
+                    // No need to resume on the original SynchronizationContext
+                    await task.ConfigureAwait(false);
+                }
+                catch
+                {
+                    // Nothing to do here
+                }
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/TaskExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Meziantou.Framework.Tests
+{
+    [TestClass]
+    public sealed class TaskExtensionsTests
+    {
+        [TestMethod]
+        public void ForgetTest_SuccessfullyCompleted()
+        {
+            var task = Task.FromResult(0);
+            task.Forget(); // Should not throw exception
+        }
+
+        [TestMethod]
+        public void ForgetTest_Faulted()
+        {
+            var task = Task.FromException(new Exception(""));
+            task.Forget(); // Should not throw exception
+        }
+
+        [TestMethod]
+        public void ForgetTest_Canceled()
+        {
+            using var cts = new CancellationTokenSource();
+            cts.Cancel();
+            var task = Task.FromCanceled(cts.Token);
+            task.Forget(); // Should not throw exception
+        }
+    }
+}

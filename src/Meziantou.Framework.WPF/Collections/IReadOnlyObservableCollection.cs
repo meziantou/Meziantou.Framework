7d71575a4f0d835f4c770c16e7d8caab6977f9b8
Gérald Barré <meziantou@users.noreply.github.com>
2019-06-28 17:53:30 -0400

    Add ObservableCollection (#19)

---
+++ src/Meziantou.Framework.WPF/Collections/IReadOnlyObservableCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace Meziantou.Framework.Windows.Collections
+{
+    public interface IReadOnlyObservableCollection<T> : IReadOnlyList<T>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+    }
+}
---
+++ src/Meziantou.Framework.WPF/Collections/PendingEvent`1.cs
@@ -0,0 +1,33 @@
+﻿namespace Meziantou.Framework.Windows.Collections
+{
+    internal sealed class PendingEvent<T>
+    {
+        public PendingEvent(PendingEventType type)
+        {
+            Type = type;
+        }
+
+        public PendingEvent(PendingEventType type, int index)
+        {
+            Type = type;
+            Index = index;
+        }
+
+        public PendingEvent(PendingEventType type, T item)
+        {
+            Type = type;
+            Item = item;
+        }
+
+        public PendingEvent(PendingEventType type, T item, int index)
+        {
+            Type = type;
+            Item = item;
+            Index = index;
+        }
+
+        public PendingEventType Type { get; }
+        public T Item { get; }
+        public int Index { get; }
+    }
+}

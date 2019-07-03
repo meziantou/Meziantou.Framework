c11b440c9756092aa25acc509873d95d98217172
meziantou <meziantou@live.fr>
2019-07-02 23:16:47 -0400

    Add sample

---
+++ Samples/Meziantou.Framework.WPF.CollectionSamples/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using Meziantou.Framework.WPF.Collections;
+
+namespace Meziantou.Framework.WPF.CollectionSamples
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ThreadSafeCollection<string> _items = new ThreadSafeCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Lbx.ItemsSource = _items.AsObservable;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() => Parallel.For(_items.Count, _items.Count + 1000, i => _items.Add(i.ToString())));
+        }
+    }
+}

d09ddc1350799956149563e7d5db1a90ad07a7ee
meziantou <meziantou@users.noreply.github.com>
2019-07-26 14:42:21 -0400

    Meziantou.Framework.Win32.Lsa.Tests

---
+++ tests/TestUtilities/RunIfAdminFactAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using Xunit;
+
+namespace TestUtilities
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class RunIfAdminFactAttribute : FactAttribute
+    {
+        public RunIfAdminFactAttribute()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Skip = "Run only on Windows";
+                return;
+            }
+
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                Skip = "Current user is not in the administator group";
+            }
+        }
+    }
+}

16f7dc760bdaf78a03957e35ffd99d8c71ab0626
meziantou <meziantou@live.fr>
2019-07-26 18:35:00 -0400

    fix CI

---
+++ tests/TestUtilities/RunIfNotOnAzurePipelineAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace TestUtilities
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class RunIfNotOnAzurePipelineAttribute : FactAttribute
+    {
+        public RunIfNotOnAzurePipelineAttribute()
+        {
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("BUILD_DEFINITIONNAME")))
+            {
+                Skip = "Skip on Azure Pipeline";
+            }
+        }
+    }
+}

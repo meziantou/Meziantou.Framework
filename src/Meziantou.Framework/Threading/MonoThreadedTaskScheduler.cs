885bede725e16f50d2f39170ab235c40200072f4
meziantou <meziantou@live.fr>
2019-08-23 17:28:56 -0400

    Add MonoThreadedTaskScheduler

---
+++ src/Meziantou.Framework/Threading/MonoThreadedTaskScheduler.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Meziantou.Framework.Threading
+{
+    public sealed class MonoThreadedTaskScheduler : TaskScheduler, IDisposable
+    {
+        private readonly ConcurrentQueue<Task> _tasks = new ConcurrentQueue<Task>();
+        private readonly AutoResetEvent _stop = new AutoResetEvent(initialState: false);
+        private readonly AutoResetEvent _dequeue = new AutoResetEvent(initialState: false);
+
+        public MonoThreadedTaskScheduler()
+            : this(threadName: null)
+        {
+        }
+
+        public MonoThreadedTaskScheduler(string threadName)
+        {
+            Thread = new Thread(SafeThreadExecute)
+            {
+                IsBackground = true,
+                Name = threadName,
+            };
+
+            Thread.Start();
+
+            DisposeThreadJoinTimeout = TimeSpan.FromMilliseconds(1000);
+            WaitTimeout = TimeSpan.FromMilliseconds(100);
+        }
+
+        private Thread Thread { get; set; }
+        public bool DequeueOnDispose { get; set; }
+        public TimeSpan DisposeThreadJoinTimeout { get; set; }
+        public TimeSpan WaitTimeout { get; set; }
+        public TimeSpan DequeueTimeout { get; set; }
+
+        public int QueueCount => _tasks.Count;
+
+        public void Dispose()
+        {
+            if (_stop != null)
+            {
+                _stop.Set();
+                _stop.Dispose();
+            }
+
+            if (_dequeue != null)
+            {
+                _dequeue.Dispose();
+            }
+
+            if (DequeueOnDispose)
+            {
+                Dequeue();
+            }
+
+            if (Thread != null && Thread.IsAlive)
+            {
+                Thread.Join(DisposeThreadJoinTimeout);
+            }
+
+            Thread = null;
+        }
+
+        private bool ExecuteTask(Task task)
+        {
+            return TryExecuteTask(task);
+        }
+
+        private void Dequeue()
+        {
+            do
+            {
+                if (!_tasks.TryDequeue(out var task))
+                    break;
+
+                ExecuteTask(task);
+            }
+            while (true);
+        }
+
+        private void SafeThreadExecute()
+        {
+            try
+            {
+                ThreadExecute();
+            }
+            catch
+            {
+            }
+        }
+
+        private void ThreadExecute()
+        {
+            do
+            {
+                // note: Stop must be first in array (in case both events happen at the same exact time)
+                var i = WaitHandle.WaitAny(new[] { _stop, _dequeue }, WaitTimeout);
+                if (i == 0)
+                    break;
+
+                // note: we can dequeue on _dequeue event, or on timeout
+                Dequeue();
+            }
+            while (true);
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks() => _tasks;
+
+        protected override void QueueTask(Task task)
+        {
+            if (task == null)
+                throw new ArgumentNullException(nameof(task));
+
+            _tasks.Enqueue(task);
+            _dequeue.Set();
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            return false;
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/Threading/MonoThreadedTaskSchedulerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Meziantou.Framework.Threading;
+using Xunit;
+
+namespace Meziantou.Framework.Tests.Threading
+{
+    public sealed class MonoThreadedTaskSchedulerTests : IDisposable
+    {
+        private const string ThreadName = "Test";
+
+        private readonly MonoThreadedTaskScheduler _taskScheduler;
+        private int _count = 0;
+
+        public MonoThreadedTaskSchedulerTests()
+        {
+            _taskScheduler = new MonoThreadedTaskScheduler(ThreadName);
+        }
+
+        public void Dispose()
+        {
+            _taskScheduler.Dispose();
+        }
+
+        [Fact]
+        public async Task SequentialEnqueue()
+        {
+            const int Count = 1000;
+            for (var i = 0; i < Count; i++)
+            {
+                await EnqueueTask();
+            }
+
+            Assert.Equal(Count, _count);
+        }
+
+        [Fact]
+        public async Task ParallelEnqueue()
+        {
+            const int Count = 1000;
+            var tasks = new Task[Count];
+            for (var i = 0; i < Count; i++)
+            {
+                tasks[i] = EnqueueTask();
+            }
+
+            await Task.WhenAll(tasks);
+            Assert.Equal(Count, _count);
+        }
+
+        private Task EnqueueTask()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Assert.Equal(ThreadName, Thread.CurrentThread.Name);
+                _count++;
+            }, CancellationToken.None, TaskCreationOptions.None, _taskScheduler);
+        }
+    }
+}

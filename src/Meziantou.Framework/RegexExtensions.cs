0d84f4da84a63fe81a02a672bc2c7e0109437f11
meziantou <meziantou@live.fr>
2019-12-15 16:49:46 -0500

    Add Regex.ReplaceAsync extension method

---
+++ src/Meziantou.Framework/RegexExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Meziantou.Framework
+{
+    public static class RegexExtensions
+    {
+        public static async Task<string> ReplaceAsync(this Regex regex, string input, Func<Match, Task<string>> replacementFn)
+        {
+            var sb = new StringBuilder();
+            var lastIndex = 0;
+
+            foreach (Match? match in regex.Matches(input))
+            {
+                Debug.Assert(match != null);
+
+                var replacement = await replacementFn(match).ConfigureAwait(false);
+                sb.Append(input, lastIndex, match.Index - lastIndex)
+                  .Append(replacement);
+
+                lastIndex = match.Index + match.Length;
+            }
+
+            sb.Append(input, lastIndex, input.Length - lastIndex);
+            return sb.ToString();
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/RegexExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Meziantou.Framework.Tests
+{
+    public sealed class RegexExtensionsTests
+    {
+        [Fact]
+        public async Task ReplaceAsync()
+        {
+            var regex = new Regex("[0-9]", RegexOptions.None, TimeSpan.FromSeconds(1));
+            var actual = await regex.ReplaceAsync("a123b", async match =>
+            {
+                await Task.Yield();
+                return "_";
+            });
+
+            Assert.Equal("a___b", actual);
+        }
+    }
+}

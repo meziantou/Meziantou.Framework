e1c962efd3d681978a0d56c010101c9c1b173785
meziantou <meziantou@live.fr>
2020-02-29 16:10:28 -0500

    Add Dispatcher.SwitchToUi()

---
+++ src/Meziantou.Framework.WPF/DispatcherExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+
+namespace Meziantou.Framework.WPF
+{
+    public static class DispatcherExtensions
+    {
+        // https://medium.com/@kevingosse/switching-back-to-the-ui-thread-in-wpf-uwp-in-modern-c-5dc1cc8efa5e
+        public static SwitchToUiAwaitable SwitchToUi(this Dispatcher dispatcher)
+        {
+            return new SwitchToUiAwaitable(dispatcher);
+        }
+
+        public struct SwitchToUiAwaitable : INotifyCompletion
+        {
+            private readonly Dispatcher _dispatcher;
+
+            public SwitchToUiAwaitable(Dispatcher dispatcher)
+            {
+                _dispatcher = dispatcher;
+            }
+
+            public SwitchToUiAwaitable GetAwaiter()
+            {
+                return this;
+            }
+
+            [SuppressMessage("Design", "MA0038:Make method static", Justification = "Cannot be static")]
+            public void GetResult()
+            {
+            }
+
+            public bool IsCompleted => _dispatcher.CheckAccess();
+
+            public void OnCompleted(Action continuation)
+            {
+                _dispatcher.BeginInvoke(continuation);
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.WPF.Tests/DispatcherExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using Xunit;
+
+namespace Meziantou.Framework.WPF.Tests
+{
+    public sealed class DispatcherExtensionsTests
+    {
+        [Fact]
+        public async Task SwitchToUi()
+        {
+            var dispatcher = Dispatcher.CurrentDispatcher;
+            var currentThreadId = Thread.CurrentThread;
+
+            var manualResetEvent = new ManualResetEventSlim();
+            var t = Task.Run(async () =>
+            {
+                Assert.NotEqual(currentThreadId, Thread.CurrentThread);
+
+                var switchTask = dispatcher.SwitchToUi();
+                manualResetEvent.Set();
+                await switchTask;
+
+                Assert.Equal(currentThreadId, Thread.CurrentThread);
+            });
+
+            manualResetEvent.Wait();
+            DoEvents();
+
+            await t;
+        }
+
+        private static void DoEvents()
+        {
+            var frame = new DispatcherFrame();
+            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, new DispatcherOperationCallback(ExitFrame), frame);
+            Dispatcher.PushFrame(frame);
+        }
+
+        private static object ExitFrame(object frame)
+        {
+            ((DispatcherFrame)frame).Continue = false;
+            return null;
+        }
+    }
+}

9ee595c2208030c6ad03ca86900663aab4bc9eda
meziantou <meziantou@live.fr>
2020-04-09 17:27:08 -0400

    Add ValueStopwatch

---
+++ tests/Meziantou.Framework.Tests/ValueStopwatchTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Meziantou.Framework.Tests
+{
+    public class ValueStopwatchTest
+    {
+        [Fact]
+        public void IsActiveIsFalseForDefaultValueStopwatch()
+        {
+            Assert.False(default(ValueStopwatch).IsActive);
+        }
+
+        [Fact]
+        public void IsActiveIsTrueWhenValueStopwatchStartedWithStartNew()
+        {
+            Assert.True(ValueStopwatch.StartNew().IsActive);
+        }
+
+        [Fact]
+        public void GetElapsedTimeThrowsIfValueStopwatchIsDefaultValue()
+        {
+            var stopwatch = default(ValueStopwatch);
+            Assert.Throws<InvalidOperationException>(() => stopwatch.GetElapsedTime());
+        }
+
+        [Fact]
+        public async Task GetElapsedTimeReturnsTimeElapsedSinceStart()
+        {
+            var stopwatch = ValueStopwatch.StartNew();
+            await Task.Delay(200);
+            Assert.True(stopwatch.GetElapsedTime().TotalMilliseconds > 0);
+        }
+    }
+
+}

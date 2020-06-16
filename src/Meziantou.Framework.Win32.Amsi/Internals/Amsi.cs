c2f7ea369ff196994542d00443c0238d4648e6df
meziantou <meziantou@live.fr>
2020-06-16 14:05:52 -0400

    Restrict loading of `amsi.dll` from system32 folder

--- src/Meziantou.Framework.Win32.Amsi/Internals/Amsi.cs
+++ src/Meziantou.Framework.Win32.Amsi/Internals/Amsi.cs
@@ -10,21 +10,27 @@ namespace Meziantou.Framework.Win32
             return result >= AmsiResult.AMSI_RESULT_DETECTED;
         }
 
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
         [DllImport("Amsi.dll", EntryPoint = "AmsiInitialize", CallingConvention = CallingConvention.StdCall)]
-        internal static extern int AmsiInitialize([MarshalAs(UnmanagedType.LPWStr)]string appName, out AmsiContextSafeHandle amsiContext);
+        internal static extern int AmsiInitialize([MarshalAs(UnmanagedType.LPWStr)] string appName, out AmsiContextSafeHandle amsiContext);
 
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
         [DllImport("Amsi.dll", EntryPoint = "AmsiUninitialize", CallingConvention = CallingConvention.StdCall)]
         internal static extern void AmsiUninitialize(IntPtr amsiContext);
 
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
         [DllImport("Amsi.dll", EntryPoint = "AmsiOpenSession", CallingConvention = CallingConvention.StdCall)]
         internal static extern int AmsiOpenSession(AmsiContextSafeHandle amsiContext, out AmsiSessionSafeHandle session);
 
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
         [DllImport("Amsi.dll", EntryPoint = "AmsiCloseSession", CallingConvention = CallingConvention.StdCall)]
         internal static extern void AmsiCloseSession(AmsiContextSafeHandle amsiContext, IntPtr session);
 
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
         [DllImport("Amsi.dll", EntryPoint = "AmsiScanString", CallingConvention = CallingConvention.StdCall)]
-        internal static extern int AmsiScanString(AmsiContextSafeHandle amsiContext, [In, MarshalAs(UnmanagedType.LPWStr)]string payload, [In, MarshalAs(UnmanagedType.LPWStr)]string contentName, AmsiSessionSafeHandle session, out AmsiResult result);
+        internal static extern int AmsiScanString(AmsiContextSafeHandle amsiContext, [In, MarshalAs(UnmanagedType.LPWStr)] string payload, [In, MarshalAs(UnmanagedType.LPWStr)] string contentName, AmsiSessionSafeHandle session, out AmsiResult result);
 
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
         [DllImport("Amsi.dll", EntryPoint = "AmsiScanBuffer", CallingConvention = CallingConvention.StdCall)]
         internal static extern int AmsiScanBuffer(AmsiContextSafeHandle amsiContext, byte[] buffer, uint length, string contentName, AmsiSessionSafeHandle session, out AmsiResult result);
     }

0a90a8ffebebd2152ed33e8bfff30ccd6de8acdd
meziantou <meziantou@live.fr>
2020-07-23 17:43:19 -0400

    Add test for temporary directory

---
+++ tests/Meziantou.Framework.Tests/TemporaryDirectoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Meziantou.Framework.Tests
+{
+    public class TemporaryDirectoryTests
+    {
+        [Fact]
+        public void CreateInParallel()
+        {
+            const int Iterations = 100;
+            var dirs = new TemporaryDirectory[Iterations];
+
+            Parallel.For(0, Iterations, new ParallelOptions { MaxDegreeOfParallelism = 50 }, i =>
+            {
+                dirs[i] = TemporaryDirectory.Create();
+            });
+
+            try
+            {
+                Assert.Equal(Iterations, dirs.DistinctBy(dir => dir.FullPath).Count());
+
+                foreach (var dir in dirs)
+                {
+                    Assert.All(dirs, dir => Assert.True(Directory.Exists(dir.FullPath)));
+                }
+            }
+            finally
+            {
+                foreach (var item in dirs)
+                {
+                    item?.Dispose();
+                }
+            }
+        }
+    }
+}

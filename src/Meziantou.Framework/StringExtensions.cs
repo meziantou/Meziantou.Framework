51d92126e5099445e34ef25b9de18301d10721f7
meziantou <meziantou@live.fr>
2020-10-06 20:11:13 -0400

    Split StringExtensions

--- src/Meziantou.Framework/StringExtensions.cs
+++ src/Meziantou.Framework/StringExtensions.cs
@@ -2,12 +2,17 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
 using System.Globalization;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Meziantou.Framework
 {
-    public static class StringExtensions
+
+#if PUBLIC_STRING_EXTENSIONS
+    public
+#else
+    internal
+#endif
+    static partial class StringExtensions
     {
         [Pure]
         public static string? Nullify(this string? str, bool trim)
@@ -103,76 +108,6 @@ namespace Meziantou.Framework
                 return false;
 
             return str[0] == c;
-        }
-
-        [Pure]
-        public static LineSplitEnumerator SplitLines(this string str) => new LineSplitEnumerator(str.AsSpan());
-
-        [StructLayout(LayoutKind.Auto)]
-        public ref struct LineSplitEnumerator
-        {
-            private ReadOnlySpan<char> _str;
-
-            public LineSplitEnumerator(ReadOnlySpan<char> str)
-            {
-                _str = str;
-                Current = default;
-            }
-
-            public LineSplitEnumerator GetEnumerator() => this;
-
-            public bool MoveNext()
-            {
-                if (_str.Length == 0)
-                    return false;
-
-                var span = _str;
-                var index = span.IndexOfAny('\r', '\n');
-                if (index == -1)
-                {
-                    _str = ReadOnlySpan<char>.Empty;
-                    Current = new LineSplitEntry(span, ReadOnlySpan<char>.Empty);
-                    return true;
-                }
-
-                if (index < span.Length - 1 && span[index] == '\r')
-                {
-                    var next = span[index + 1];
-                    if (next == '\n')
-                    {
-                        Current = new LineSplitEntry(span.Slice(0, index), span.Slice(index, 2));
-                        _str = span[(index + 2)..];
-                        return true;
-                    }
-                }
-
-                Current = new LineSplitEntry(span.Slice(0, index), span.Slice(index, 1));
-                _str = span[(index + 1)..];
-                return true;
-            }
-
-            public LineSplitEntry Current { get; private set; }
-        }
-
-        [StructLayout(LayoutKind.Auto)]
-        public readonly ref struct LineSplitEntry
-        {
-            public LineSplitEntry(ReadOnlySpan<char> line, ReadOnlySpan<char> separator)
-            {
-                Line = line;
-                Separator = separator;
-            }
-
-            public ReadOnlySpan<char> Line { get; }
-            public ReadOnlySpan<char> Separator { get; }
-
-            public void Deconstruct(out ReadOnlySpan<char> line, out ReadOnlySpan<char> separator)
-            {
-                line = Line;
-                separator = Separator;
-            }
-
-            public static implicit operator ReadOnlySpan<char>(LineSplitEntry entry) => entry.Line;
         }
     }
 }

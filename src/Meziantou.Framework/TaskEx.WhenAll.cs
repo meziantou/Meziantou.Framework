b60a0accfb4fa9f58b3cc7ce05ac59a1e7f7a809
meziantou <meziantou@live.fr>
2020-12-06 13:49:30 -0500

    Add TaskEx.GetAwaiter(ValueTuple) to await ValueTuple directory

--- src/Meziantou.Framework/TaskEx.WhenAll.cs
+++ src/Meziantou.Framework/TaskEx.WhenAll.cs
@@ -1,10 +1,12 @@
 ﻿#pragma warning disable MA0042 // Do not use blocking call
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Meziantou.Framework
 {
+    // https://www.meziantou.net/get-the-result-of-multiple-tasks-in-a-valuetuple-and-whenall.htm
     public static partial class TaskEx
     {
         public static async Task<(T0, T1)> WhenAll<T0, T1>(Task<T0> task0, Task<T1> task1)
@@ -101,6 +103,36 @@ namespace Meziantou.Framework
 
             await Task.WhenAll(task0, task1, task2, task3, task4, task5, task6).ConfigureAwait(false);
             return (task0.Result, task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result);
+        }
+
+        public static TaskAwaiter<(T1, T2)> GetAwaiter<T1, T2>(this ValueTuple<Task<T1>, Task<T2>> tasks)
+        {
+            return WhenAll(tasks.Item1, tasks.Item2).GetAwaiter();
+        }
+
+        public static TaskAwaiter<(T1, T2, T3)> GetAwaiter<T1, T2, T3>(this ValueTuple<Task<T1>, Task<T2>, Task<T3>> tasks)
+        {
+            return WhenAll(tasks.Item1, tasks.Item2, tasks.Item3).GetAwaiter();
+        }
+
+        public static TaskAwaiter<(T1, T2, T3, T4)> GetAwaiter<T1, T2, T3, T4>(this ValueTuple<Task<T1>, Task<T2>, Task<T3>, Task<T4>> tasks)
+        {
+            return WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4).GetAwaiter();
+        }
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5)> GetAwaiter<T1, T2, T3, T4, T5>(this ValueTuple<Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>> tasks)
+        {
+            return WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5).GetAwaiter();
+        }
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5, T6)> GetAwaiter<T1, T2, T3, T4, T5, T6>(this ValueTuple<Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>, Task<T6>> tasks)
+        {
+            return WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5, tasks.Item6).GetAwaiter();
+        }
+
+        public static TaskAwaiter<(T1, T2, T3, T4, T5, T6, T7)> GetAwaiter<T1, T2, T3, T4, T5, T6, T7>(this ValueTuple<Task<T1>, Task<T2>, Task<T3>, Task<T4>, Task<T5>, Task<T6>, Task<T7>> tasks)
+        {
+            return WhenAll(tasks.Item1, tasks.Item2, tasks.Item3, tasks.Item4, tasks.Item5, tasks.Item6, tasks.Item7).GetAwaiter();
         }
     }
 }

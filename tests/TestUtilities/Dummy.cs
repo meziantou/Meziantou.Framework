51220d21d0c878ef900f12e85e9e054f744244eb
meziantou <meziantou@live.fr>
2020-12-26 18:06:59 -0500

    Use GitHub Actions

---
+++ tests/TestUtilities/Dummy.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace TestUtilities
+{
+    public sealed class Dummy
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Dummy(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact(Skip = "For testing purpose")]
+        public void Test()
+        {
+            _outputHelper.WriteLine("This test output one line");
+            Assert.True(false, "This is a dummy test");
+        }
+    }
+}
---
+++ tests/TestUtilities/RunIfNotOnCIFactAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace TestUtilities
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class RunIfNotOnCIFactAttribute : FactAttribute
+    {
+        public RunIfNotOnCIFactAttribute()
+        {
+            if (IsOnCI())
+            {
+                Skip = "Skip on CI";
+            }
+        }
+
+        public static bool IsOnCI()
+        {
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("BUILD_DEFINITIONNAME")))
+                return true;
+
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("GITHUB_ACTIONS")))
+                return true;
+
+            return false;
+        }
+    }
+}

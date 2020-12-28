6c785a271c61efa547597a12abb827ca3c773f8e
meziantou <meziantou@live.fr>
2020-12-27 21:07:14 -0500

    Add more testing frameworks

---
+++ tests/TestUtilities/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿#if NET461
+using System;
+
+namespace TestUtilities
+{
+    internal static class StringExtensions
+    {
+        public static string Replace(this string str, string oldValue, string newValue, StringComparison stringComparison)
+        {
+            var working = str;
+            var index = working.IndexOf(oldValue, stringComparison);
+            while (index != -1)
+            {
+                working = working.Remove(index, oldValue.Length);
+                working = working.Insert(index, newValue);
+                index += newValue.Length;
+                index = working.IndexOf(oldValue, index, stringComparison);
+            }
+
+            return working;
+        }
+    }
+}
+#endif

5e2846cecbad28736598975d5b376f4dca4fbb06
meziantou <meziantou@live.fr>
2020-12-28 12:56:19 -0500

    Add BooleanToValueConverter

---
+++ src/Meziantou.Framework.WPF/BooleanToValueConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Meziantou.Framework.WPF
+{
+    public sealed class BooleanToValueConverter : IValueConverter
+    {
+        public object? TrueValue { get; set; }
+        public object? FalseValue { get; set; }
+
+        /// <summary>
+        /// If not set, the converter use the <see cref="FalseValue"/> instead
+        /// </summary>
+        public object? NullValue { get; set; }
+
+        private object? GetValue(bool value)
+        {
+            return value ? TrueValue : FalseValue;
+        }
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo? culture)
+        {
+            if (value == null)
+                return NullValue ?? FalseValue;
+
+            if (value is bool b)
+                return GetValue(b);
+
+            if (value is IConvertible convertible)
+            {
+                try
+                {
+                    return GetValue(convertible.ToBoolean(culture));
+                }
+                catch (FormatException)
+                {
+                }
+                catch (InvalidCastException)
+                {
+                }
+            }
+
+            return NullValue;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.WPF.Tests/BooleanToValueConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+
+namespace Meziantou.Framework.WPF.Tests
+{
+    public sealed class BooleanToValueConverterTests
+    {
+        private readonly BooleanToValueConverter _converter = new()
+        {
+            TrueValue = "true",
+            FalseValue = "false",
+            NullValue = "null",
+        };
+
+        private object Convert(object value)
+        {
+            return _converter.Convert(value, typeof(object), parameter: null, culture: null);
+        }
+
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(true, "true")]
+        [InlineData(true, 1)]
+        [InlineData(true, 1u)]
+        [InlineData(true, -1)]
+        [InlineData(false, false)]
+        [InlineData(false, 0)]
+        [InlineData(false, "false")]
+        [InlineData(null, null)]
+        [InlineData(null, "")]
+        [InlineData(null, "abc")]
+        public void Test(bool? expectedValue, object value)
+        {
+            var expected = expectedValue switch
+            {
+                null => _converter.NullValue,
+                false => _converter.FalseValue,
+                true => _converter.TrueValue,
+            };
+
+            Assert.Equal(expected, Convert(value));
+        }
+
+        [Fact]
+        public void FallbackToFalseValue()
+        {
+            _converter.NullValue = null;
+
+            Assert.Equal(_converter.FalseValue, Convert(null));
+        }
+    }
+}

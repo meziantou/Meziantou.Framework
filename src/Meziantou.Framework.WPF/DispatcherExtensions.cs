2907de9c1eda688beba86b4ce13e0a9cb54346b3
meziantou <meziantou@live.fr>
2021-01-05 13:31:41 -0500

    Update analyzers

--- src/Meziantou.Framework.WPF/DispatcherExtensions.cs
+++ src/Meziantou.Framework.WPF/DispatcherExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Windows.Threading;
 
@@ -22,13 +21,11 @@ namespace Meziantou.Framework.WPF
                 _dispatcher = dispatcher;
             }
 
-            [SuppressMessage("Design", "CA1024:Use properties where appropriate", Justification = "Must be a method to be awaitable")]
             public SwitchToUiAwaitable GetAwaiter()
             {
                 return this;
             }
 
-            [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Cannot be static for the await pattern")]
             public void GetResult()
             {
             }

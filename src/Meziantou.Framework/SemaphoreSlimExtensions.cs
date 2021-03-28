236666619aaa3402ce2d6ebe4d57113d25e2812d
Gérald Barré <meziantou@live.fr>
2021-03-28 12:29:49 -0400

    Use local tools for CI

--- src/Meziantou.Framework/SemaphoreSlimExtensions.cs
+++ src/Meziantou.Framework/SemaphoreSlimExtensions.cs
@@ -6,16 +6,48 @@ namespace Meziantou.Framework
 {
     public static class SemaphoreSlimExtensions
     {
-        public static SemaphoreDisposer DisposableWait(this SemaphoreSlim semaphore, CancellationToken cancellationToken = default)
+        public static SemaphoreDisposer DisposableUnsafeWait(this SemaphoreSlim semaphore, CancellationToken cancellationToken = default)
         {
             semaphore.Wait(cancellationToken);
             return new SemaphoreDisposer(semaphore);
         }
 
-        public static async ValueTask<SemaphoreDisposer> DisposableWaitAsync(this SemaphoreSlim semaphore, CancellationToken cancellationToken = default)
+        public static async Task<SemaphoreDisposer> DisposableWaitUnsafeAsync(this SemaphoreSlim semaphore, CancellationToken cancellationToken = default)
         {
             await semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
             return new SemaphoreDisposer(semaphore);
+        }
+
+        public static IDisposable DisposableWait(this SemaphoreSlim semaphore, CancellationToken cancellationToken = default)
+        {
+            semaphore.Wait(cancellationToken);
+            return new SemaphoreDisposerClass(semaphore);
+        }
+
+        public static async Task<IDisposable> DisposableWaitAsync(this SemaphoreSlim semaphore, CancellationToken cancellationToken = default)
+        {
+            await semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+            return new SemaphoreDisposerClass(semaphore);
+        }
+
+        private sealed class SemaphoreDisposerClass : IDisposable
+        {
+            private readonly SemaphoreSlim _semaphore;
+            private bool _disposed;
+
+            public SemaphoreDisposerClass(SemaphoreSlim semaphore)
+            {
+                _semaphore = semaphore;
+            }
+
+            public void Dispose()
+            {
+                if (!_disposed)
+                {
+                    _semaphore.Release();
+                    _disposed = true;
+                }
+            }
         }
 
         public readonly struct SemaphoreDisposer : IDisposable

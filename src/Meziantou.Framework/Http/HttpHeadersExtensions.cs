0d014a993a5e70b10e9aa2d1751026d3efbd2004
Gérald Barré <meziantou@live.fr>
2021-09-24 23:06:01 -0400

    Add http link header parser

---
+++ src/Meziantou.Framework/Http/HttpHeadersExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Meziantou.Framework.Http
+{
+    public static class HttpHeadersExtensions
+    {
+        public static IEnumerable<LinkHeaderValue> ParseLinkHeaders(this HttpHeaders headers)
+        {
+            if (headers.TryGetValues("link", out var values))
+            {
+                foreach (var value in values)
+                {
+                    foreach (var linkValue in LinkHeaderValue.Parse(value))
+                    {
+                        yield return linkValue;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/Http/LinkHeaderValueTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Headers;
+using FluentAssertions;
+using Meziantou.Framework.Http;
+using Xunit;
+
+namespace Meziantou.Framework.Tests.Http
+{
+    public sealed class LinkHeaderValueTests
+    {
+        [Fact]
+        public void LinkHeaderValue_Parse()
+        {
+            var result = LinkHeaderValue.Parse("<sample>; rel=abc, <plop>; rel\t=\"d\\\"e;f,\"; title = test title; abc");
+            result.Should().SatisfyRespectively(
+                item =>
+                {
+                    item.Url.Should().Be("sample");
+                    item.Rel.Should().Be("abc");
+                },
+                item =>
+                {
+                    item.Url.Should().Be("plop");
+                    item.Rel.Should().Be("d\"e;f,");
+                    item.GetParameterValue("title").Should().Be("test title");
+                    item.GetParameterValue("abc").Should().Be("");
+                    item.GetParameterValue("unknown").Should().BeNull();
+                });
+        }
+
+        [Fact]
+        public void HttpResponse_Links()
+        {
+            var header = new CustomHttpHeaders();
+            header.Add("link", "<a>;rel=b, <c>; rel=d");
+            header.Add("link", "<e>;rel=f");
+
+            header.ParseLinkHeaders().Should().HaveCount(3);
+        }
+
+        private sealed class CustomHttpHeaders : HttpHeaders
+        {
+        }
+    }
+}

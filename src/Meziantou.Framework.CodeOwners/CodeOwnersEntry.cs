0fc6fb9be4a0833729db568c1b6f544b6a7c11d7
Gérald Barré <meziantou@live.fr>
2021-10-20 20:49:02 -0400

    Parse sections in CODEOWNERS

--- src/Meziantou.Framework.CodeOwners/CodeOwnersEntry.cs
+++ src/Meziantou.Framework.CodeOwners/CodeOwnersEntry.cs
@@ -6,10 +6,11 @@ namespace Meziantou.Framework.CodeOwners
     [StructLayout(LayoutKind.Auto)]
     public readonly struct CodeOwnersEntry : IEquatable<CodeOwnersEntry>
     {
-        private CodeOwnersEntry(string pattern, CodeOwnersEntryType entryType, string member)
+        private CodeOwnersEntry(string pattern, CodeOwnersEntryType entryType, string member, CodeOwnersSection? section)
         {
             Pattern = pattern ?? throw new ArgumentNullException(nameof(pattern));
             Member = member ?? throw new ArgumentNullException(nameof(member));
+            Section = section;
             EntryType = entryType;
         }
 
@@ -19,14 +20,16 @@ namespace Meziantou.Framework.CodeOwners
 
         public string Member { get; }
 
-        internal static CodeOwnersEntry FromUsername(string pattern, string username)
+        public CodeOwnersSection? Section { get; }
+
+        internal static CodeOwnersEntry FromUsername(string pattern, string username, CodeOwnersSection? section)
         {
-            return new CodeOwnersEntry(pattern, CodeOwnersEntryType.Username, username);
+            return new CodeOwnersEntry(pattern, CodeOwnersEntryType.Username, username, section);
         }
 
-        internal static CodeOwnersEntry FromEmailAddress(string pattern, string emailAddress)
+        internal static CodeOwnersEntry FromEmailAddress(string pattern, string emailAddress, CodeOwnersSection? section)
         {
-            return new CodeOwnersEntry(pattern, CodeOwnersEntryType.EmailAddress, emailAddress);
+            return new CodeOwnersEntry(pattern, CodeOwnersEntryType.EmailAddress, emailAddress, section);
         }
 
         public override bool Equals(object? obj)
@@ -39,7 +42,8 @@ namespace Meziantou.Framework.CodeOwners
         {
             return EntryType == other.EntryType &&
                    string.Equals(Pattern, other.Pattern, StringComparison.Ordinal) &&
-                   string.Equals(Member, other.Member, StringComparison.Ordinal);
+                   string.Equals(Member, other.Member, StringComparison.Ordinal) &&
+                   Section == other.Section;
         }
 
         public override int GetHashCode()
@@ -48,6 +52,7 @@ namespace Meziantou.Framework.CodeOwners
             hashCode = hashCode * -1521134295 + EntryType.GetHashCode();
             hashCode = hashCode * -1521134295 + StringComparer.Ordinal.GetHashCode(Pattern);
             hashCode = hashCode * -1521134295 + StringComparer.Ordinal.GetHashCode(Member);
+            hashCode = (hashCode * -1521134295) + (Section == null ? 0 : Section.GetHashCode());
             return hashCode;
         }
 
---
+++ src/Meziantou.Framework.CodeOwners/CodeOwnersSection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Meziantou.Framework.CodeOwners
+{
+    [StructLayout(LayoutKind.Auto)]
+    public readonly struct CodeOwnersSection : IEquatable<CodeOwnersSection>
+    {
+        public CodeOwnersSection(string name, bool isOptional)
+        {
+            Name = name;
+            IsOptional = isOptional;
+        }
+
+        public string Name { get; }
+        public bool IsOptional { get; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CodeOwnersSection section && Equals(section);
+        }
+
+        public bool Equals(CodeOwnersSection other)
+        {
+            return Name == other.Name &&
+                   IsOptional == other.IsOptional;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1707150943;
+            hashCode = hashCode * -1521134295 + IsOptional.GetHashCode();
+            hashCode = hashCode * -1521134295 + StringComparer.Ordinal.GetHashCode(Name);
+            return hashCode;
+        }
+
+        public static bool operator ==(CodeOwnersSection left, CodeOwnersSection right) => left.Equals(right);
+        public static bool operator !=(CodeOwnersSection left, CodeOwnersSection right) => !(left == right);
+    }
+}

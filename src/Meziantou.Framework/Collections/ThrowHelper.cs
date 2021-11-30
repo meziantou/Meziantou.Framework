04a9fce23d7bd3d86d021f75ff38f9cc2aee4a6e
Gérald Barré <meziantou@live.fr>
2021-11-30 00:41:36 -0500

    Add SortedList<T>

---
+++ src/Meziantou.Framework/Collections/ThrowHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Meziantou.Framework.Collections
+{
+    internal static class ThrowHelper
+    {
+        [DoesNotReturn]
+        [SuppressMessage("Usage", "MA0015:Specify the parameter name in ArgumentException", Justification = "The parameter name is a constant")]
+        internal static void ThrowArgumentOutOfRange_IndexException()
+        {
+            throw new ArgumentOutOfRangeException("index", "Index was out of range. Must be non-negative and less than the size of the collection.");
+        }
+
+        [DoesNotReturn]
+        internal static void ThrowInvalidOperationException_InvalidOperation_EnumFailedVersion()
+        {
+            throw new InvalidOperationException("Collection was modified after the enumerator was instantiated.");
+        }
+
+        [DoesNotReturn]
+        internal static void ThrowInvalidOperationException_InvalidOperation_EnumOpCantHappen()
+        {
+            throw new InvalidOperationException("Enumeration has either not started or has already finished.");
+        }
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/Collections/SortedListTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Meziantou.Framework.Collections;
+using Xunit;
+
+namespace Meziantou.Framework.Tests.Collections
+{
+    public sealed class SortedListTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var list = new SortedList<int> { 1, 3, 2 };
+            list.Should().Equal(new[] { 1, 2, 3 });
+
+            list.IndexOf(1).Should().Be(0);
+            list.IndexOf(2).Should().Be(1);
+            list.IndexOf(42).Should().Be(-1);
+
+            list.Remove(2);
+            list.Should().Equal(new[] { 1, 3 });
+
+            list.Remove(1);
+            list.Should().Equal(new[] { 3 });
+
+            list.Remove(3);
+            list.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void AsSpan()
+        {
+            var list = new SortedList<int> { 1, 3, 2 };
+
+            list.UnsafeAsReadOnlySpan().Length.Should().Be(3);
+        }
+    }
+}

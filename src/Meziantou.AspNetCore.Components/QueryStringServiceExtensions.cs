5394b9469ce6684322e28d1a05b9dbdc5659d3ee
Gérald Barré <meziantou@live.fr>
2021-12-30 23:08:26 -0500

    Restore QueryStringService

---
+++ src/Meziantou.AspNetCore.Components/QueryStringServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Meziantou.AspNetCore.Components
+{
+    public static class QueryStringServiceExtensions
+    {
+        public static IServiceCollection AddQueryStringParameters(this IServiceCollection services)
+        {
+            services.AddScoped<QueryStringService>();
+            return services;
+        }
+    }
+}

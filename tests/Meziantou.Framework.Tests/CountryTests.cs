9bf1bef9d11a2136a4380d18af08cea77b7ea8cd
Gérald Barré <meziantou@live.fr>
2022-02-28 16:21:49 -0500

    Add  Country.GetUnicodeFlag

---
+++ tests/Meziantou.Framework.Tests/CountryTests.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using FluentAssertions;
+using TestUtilities;
+using Xunit;
+
+namespace Meziantou.Framework.Tests;
+
+public sealed class CountryTests
+{
+    [Theory]
+    [InlineData("fr")]
+    [InlineData("FR")]
+    [InlineData("fR")]
+    [InlineData("Fr")]
+    public void FrenchFlag(string name)
+    {
+        Country.GetUnicodeFlag(name).Should().Be("\U0001F1EB\U0001F1F7");
+        Country.GetUnicodeFlag(name).Should().Be("\U0001F1EB\U0001F1F7");
+    }
+
+    [RunIfFact(globalizationMode: FactInvariantGlobalizationMode.Disabled)]
+    public void FrenchFlagFromRegion()
+    {
+        Country.GetUnicodeFlag(new RegionInfo("FR")).Should().Be("\U0001F1EB\U0001F1F7");
+    }
+}

05e447b070345856d2b9c5f3355024798f13ce80
Gérald Barré <meziantou@live.fr>
2022-04-09 23:13:43 -0400

    Add ObjectGraphVisitor

---
+++ src/Meziantou.Framework/ObjectGraphVisitor.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Reflection;
+
+namespace Meziantou.Framework;
+
+public abstract class ObjectGraphVisitor
+{
+    [RequiresUnreferencedCode("ObjectGraphVisitor uses reflection")]
+    public void Visit(object? obj)
+    {
+        if (obj is null)
+            return;
+
+        var hashSet = new HashSet<object>();
+        Visit(hashSet, obj);
+    }
+
+    [RequiresUnreferencedCode("ObjectGraphVisitor uses reflection")]
+    private void Visit(HashSet<object> visitedObjects, object? obj)
+    {
+        if (obj is null)
+            return;
+
+        if (!visitedObjects.Add(obj))
+            return;
+
+        VisitValue(obj);
+
+        if (obj is string) // string is IEnumerable, and we don't want to visit it
+            return;
+
+        if (obj is IEnumerable enumerable)
+        {
+            foreach (var item in enumerable)
+            {
+                Visit(visitedObjects, item);
+            }
+        }
+        else
+        {
+            var type = obj.GetType();
+            foreach (var prop in type.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+            {
+                if (!prop.CanRead)
+                    continue;
+
+                var propValue = prop.GetValue(obj);
+                VisitProperty(obj, prop, propValue);
+                Visit(visitedObjects, propValue);
+            }
+        }
+    }
+
+    protected virtual void VisitProperty(object parentInstance, PropertyInfo property, object? propertyValue)
+    {
+    }
+
+    protected virtual void VisitValue(object value)
+    {
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/ObjectGraphVisitorTests.cs
@@ -0,0 +1,47 @@
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+
+namespace Meziantou.Framework.Tests;
+
+public sealed class ObjectGraphVisitorTests
+{
+    [Fact]
+    public void VisitAnonymousObject()
+    {
+        var visitor = new TestObjectGraphVisitor();
+        visitor.Visit(new { A = 0, B = new object[] { "abc", 1u } });
+
+        visitor.VisitedValues.Should().Contain(new object[] { 0, "abc", 1u });
+    }
+
+    [Fact]
+    public void VisitRecursiveObject()
+    {
+        var visitor = new TestObjectGraphVisitor();
+        var root = new Recursive("a", null);
+        var child = new Recursive("b", root);
+
+        visitor.Visit(child);
+
+        visitor.VisitedValues.Should().Contain(new object[] { "a", "b" });
+    }
+
+    private record Recursive(object Value, Recursive Parent);
+
+    private sealed class TestObjectGraphVisitor : ObjectGraphVisitor
+    {
+        public List<PropertyInfo> VisitedProperties { get; } = new();
+        public List<object> VisitedValues { get; } = new();
+
+        protected override void VisitProperty(object parentInstance, PropertyInfo property, object value)
+        {
+            VisitedProperties.Add(property);
+        }
+
+        protected override void VisitValue(object value)
+        {
+            VisitedValues.Add(value);
+        }
+    }
+}

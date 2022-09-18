659ec5e26201155c37b08fe7e97ea7a884ea6da5
Gérald Barré <git@meziantou.net>
2022-09-18 13:54:27 -0400

    Add MarkdownBuilder.Escape

---
+++ src/Meziantou.Framework/MarkdownBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace Meziantou.Framework;
+
+public static class MarkdownBuilder
+{
+    public static string Escape(string value)
+    {
+        var sb = new StringBuilder();
+        for (var i = 0; i < value.Length; i++)
+        {
+            var c = value[i];
+            switch (c)
+            {
+                // https://spec.commonmark.org/0.30/#backslash-escapes
+                // Any ASCII punctuation character may be backslash-escaped:
+                // -./:;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~
+                case '!':
+                case '"':
+                case '#':
+                case '$':
+                case '%':
+                case '&':
+                case '\'':
+                case '(':
+                case ')':
+                case '*':
+                case '+':
+                case ',':
+                case '-':
+                case '.':
+                case '/':
+                case ':':
+                case ';':
+                case '<':
+                case '=':
+                case '>':
+                case '?':
+                case '@':
+                case '[':
+                case '\\':
+                case ']':
+                case '^':
+                case '_':
+                case '`':
+                case '{':
+                case '|':
+                case '}':
+                case '~':
+                    sb.Append('\\');
+                    sb.Append(c);
+                    break;
+
+                default:
+                    sb.Append(c);
+                    break;
+            }
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/MarkdownBuilderTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Meziantou.Framework.Tests;
+public sealed class MarkdownBuilderTests
+{
+    private static string EscapeAndConvertToHtml(string text)
+    {
+        var escaped = MarkdownBuilder.Escape(text);
+        var html = Markdig.Markdown.ToHtml(escaped);
+        return html;
+    }
+
+    [Theory]
+    [InlineData("*test*", "<p>*test*</p>\n")]
+    [InlineData("**test**", "<p>**test**</p>\n")]
+    [InlineData("***test***", "<p>***test***</p>\n")]
+    [InlineData("_test_", "<p>_test_</p>\n")]
+    [InlineData("~test~", "<p>~test~</p>\n")]
+    [InlineData("1. test", "<p>1. test</p>\n")]
+    [InlineData("- test", "<p>- test</p>\n")]
+    [InlineData("* test", "<p>* test</p>\n")]
+    [InlineData("> test", "<p>&gt; test</p>\n")]
+    [InlineData("[test](url)", "<p>[test](url)</p>\n")]
+    [InlineData("![test](url)", "<p>![test](url)</p>\n")]
+    [InlineData("&quot;", "<p>&amp;quot;</p>\n")]
+    [InlineData("# header", "<p># header</p>\n")]
+    [InlineData("## header", "<p>## header</p>\n")]
+    [InlineData("---", "<p>---</p>\n")]
+    [InlineData("`code`", "<p>`code`</p>\n")]
+    public void Escape(string text, string expected)
+    {
+        var html = EscapeAndConvertToHtml(text);
+        html.Should().Be(expected);
+    }
+    [Theory]
+    [InlineData("test)test", "<p><a href=\"test)test\">test</a></p>\n")]
+    public void EscapeInLinkUrl(string url, string expected)
+    {
+        var markdown = $"[test]({MarkdownBuilder.Escape(url)})";
+        var html = Markdig.Markdown.ToHtml(markdown);
+        html.Should().Be(expected);
+    }
+}

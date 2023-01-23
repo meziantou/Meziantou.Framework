69a8858c29a17d901211aaa35fc0c08e1d8b2a9e
Gérald Barré <git@meziantou.net>
2023-01-22 23:38:22 -0500

    Add Meziantou.Framework.SensitiveData

---
+++ src/Meziantou.Framework.SensitiveData/Interop.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Meziantou.Framework;
+
+
---
+++ src/Meziantou.Framework.SensitiveData/SensitiveDataTypeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace Meziantou.Framework;
+
+[SuppressMessage("Usage", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated dynamically")]
+internal sealed class SensitiveDataTypeConverter : TypeConverter
+{
+    public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+    {
+        return sourceType == typeof(string);
+    }
+
+    public override bool CanConvertTo(ITypeDescriptorContext? context, [NotNullWhen(true)] Type? destinationType)
+    {
+        return false;
+    }
+
+    public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
+    {
+        throw new InvalidOperationException();
+    }
+
+    public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+    {
+        if (value is string str)
+        {
+            return SensitiveData.Create(str);
+        }
+
+        return base.ConvertFrom(context, culture, value);
+    }
+}

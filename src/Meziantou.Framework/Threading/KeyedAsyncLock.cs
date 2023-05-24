0ee57366f9f312c3a7cf87702e0797978cdf5f46
Gérald Barré <git@meziantou.net>
2023-05-24 17:42:16 -0400

    Add Keyed Locks

---
+++ src/Meziantou.Framework/Threading/KeyedAsyncLock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+
+namespace Meziantou.Framework.Threading;
+
+public sealed class KeyedAsyncLock<TKey> where TKey : notnull
+{
+    private readonly ConcurrentDictionary<TKey, AsyncLock> _locks = new();
+
+    public ValueTask<AsyncLockObject> Lock(TKey key, CancellationToken cancellationToken = default)
+    {
+        var instance = _locks.GetOrAdd(key, _ => new AsyncLock());
+        return instance.LockAsync(cancellationToken);
+    }
+}
---
+++ src/Meziantou.Framework/Threading/KeyedLock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+
+namespace Meziantou.Framework.Threading;
+public sealed class KeyedLock<TKey> where TKey : notnull
+{
+    private readonly ConcurrentDictionary<TKey, object> _locks = new();
+
+    public IDisposable Lock(TKey key)
+    {
+        var instance = _locks.GetOrAdd(key, _ => new object());
+        Monitor.Enter(instance);
+        return new LockLease(instance);
+    }
+
+    private sealed class LockLease : IDisposable
+    {
+        private readonly object _lockedInstance;
+        private bool _disposed;
+
+        public LockLease(object lockedInstance)
+        {
+            _lockedInstance = lockedInstance;
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                Monitor.Exit(_lockedInstance);
+                _disposed = true;
+            }
+        }
+    }
+}

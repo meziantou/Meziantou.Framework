f45077a5b53bb2e3ef4baa264ad2156f15a90605
Gérald Barré <git@meziantou.net>
2023-05-24 21:04:50 -0400

    Rename method

--- src/Meziantou.Framework/Threading/KeyedAsyncLock.cs
+++ src/Meziantou.Framework/Threading/KeyedAsyncLock.cs
@@ -6,7 +6,7 @@ public sealed class KeyedAsyncLock<TKey> where TKey : notnull
 {
     private readonly ConcurrentDictionary<TKey, AsyncLock> _locks = new();
 
-    public ValueTask<AsyncLockObject> Lock(TKey key, CancellationToken cancellationToken = default)
+    public ValueTask<AsyncLockObject> LockAsync(TKey key, CancellationToken cancellationToken = default)
     {
         var instance = _locks.GetOrAdd(key, _ => new AsyncLock());
         return instance.LockAsync(cancellationToken);

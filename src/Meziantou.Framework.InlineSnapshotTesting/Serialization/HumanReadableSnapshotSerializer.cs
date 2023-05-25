dad3679db230d35ec401697dbeb352bd76d83e48
Gérald Barré <git@meziantou.net>
2023-05-24 21:34:12 -0400

    Make constructor public

--- src/Meziantou.Framework.InlineSnapshotTesting/Serialization/HumanReadableSnapshotSerializer.cs
+++ src/Meziantou.Framework.InlineSnapshotTesting/Serialization/HumanReadableSnapshotSerializer.cs
@@ -4,14 +4,17 @@ namespace Meziantou.Framework.InlineSnapshotTesting.Serialization;
 
 public sealed class HumanReadableSnapshotSerializer : SnapshotSerializer
 {
-    internal static HumanReadableSnapshotSerializer Instance { get; } = new();
+    private readonly HumanReadableSerializerOptions? _options;
 
-    private HumanReadableSnapshotSerializer()
+    internal static HumanReadableSnapshotSerializer Instance { get; } = new(options: null);
+
+    public HumanReadableSnapshotSerializer(HumanReadableSerializerOptions? options = null)
     {
+        _options = options;
     }
 
     public override string Serialize(object? value)
     {
-        return HumanReadableSerializer.Serialize(value);
+        return HumanReadableSerializer.Serialize(value, _options);
     }
 }

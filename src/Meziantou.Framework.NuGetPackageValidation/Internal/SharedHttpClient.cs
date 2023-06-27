9b57037d625d1ddb5878ad38dcfb80f74a8e45ca
Atif Aziz <code@raboof.com>
2023-06-27 20:13:09 +0200

    Retry HTTP request with delay hint (on 429)

--- src/Meziantou.Framework.NuGetPackageValidation/Internal/SharedHttpClient.cs
+++ src/Meziantou.Framework.NuGetPackageValidation/Internal/SharedHttpClient.cs
@@ -26,13 +26,26 @@ internal static class SharedHttpClient
             const int MaxRetries = 5;
             for (var i = 1; ; i++)
             {
+                TimeSpan? delay = null;
                 HttpResponseMessage? result = null;
+
                 try
                 {
                     result = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-                    if (!IsLastAttempt(i) && ((int)result.StatusCode >= 500 || result.StatusCode is System.Net.HttpStatusCode.RequestTimeout))
+                    if (!IsLastAttempt(i) && ((int)result.StatusCode >= 500 || result.StatusCode is System.Net.HttpStatusCode.RequestTimeout or System.Net.HttpStatusCode.TooManyRequests))
                     {
                         result.Dispose();
+
+                        // Use "Retry-After" value, if available. Typically, this is sent with
+                        // either a 503 (Service Unavailable) or 429 (Too Many Requests):
+                        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After
+
+                        delay = result.Headers.RetryAfter switch
+                        {
+                            { Date : { } date  } => date - DateTimeOffset.UtcNow,
+                            { Delta: { } delta } => delta,
+                            _ => null,
+                        };
                     }
                     else
                     {
@@ -46,7 +59,7 @@ internal static class SharedHttpClient
                         throw;
                 }
 
-                await Task.Delay(100 * i, cancellationToken).ConfigureAwait(false);
+                await Task.Delay(delay is { } someDelay && someDelay > TimeSpan.Zero ? someDelay : TimeSpan.FromMicroseconds(100 * i), cancellationToken).ConfigureAwait(false);
 
                 static bool IsLastAttempt(int i) => i >= MaxRetries;
             }

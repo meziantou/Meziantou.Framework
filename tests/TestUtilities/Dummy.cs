f730e319ebcf78a78f246111fd5c7bb36c01aa3b
Gérald Barré <git@meziantou.net>
2024-02-01 11:39:36 -0500

    Fix build warning

--- tests/TestUtilities/Dummy.cs
+++ tests/TestUtilities/Dummy.cs
@@ -1,23 +1,15 @@
-using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace TestUtilities;
 
-public sealed class Dummy
+public sealed class Dummy(ITestOutputHelper outputHelper)
 {
-    private readonly ITestOutputHelper _outputHelper;
-
-    public Dummy(ITestOutputHelper outputHelper)
-    {
-        _outputHelper = outputHelper;
-    }
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped", Justification = "<Pending>")]
+    [SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped")]
     [Fact(Skip = "For testing purpose")]
     public void Test()
     {
-        _outputHelper.WriteLine("This test output one line");
-        false.Should().BeTrue("This is a dummy test");
+        outputHelper.WriteLine("This test output one line");
+        Assert.Fail("This is a dummy test");
     }
 }

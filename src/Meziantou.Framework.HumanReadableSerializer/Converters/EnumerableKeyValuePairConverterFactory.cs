b42e6e76595393c22d546204cdab77fc13a00a9d
Gérald Barré <git@meziantou.net>
2024-04-27 12:16:52 -0400

    Improve IAsyncEnumerable support

--- src/Meziantou.Framework.HumanReadableSerializer/Converters/EnumerableKeyValuePairConverterFactory.cs
+++ src/Meziantou.Framework.HumanReadableSerializer/Converters/EnumerableKeyValuePairConverterFactory.cs
@@ -38,39 +38,4 @@ internal sealed class EnumerableKeyValuePairConverterFactory : HumanReadableConv
 
         return null;
     }
-
-    [SuppressMessage("Performance", "CA1812", Justification = "The class is instantiated using Activator.CreateInstance")]
-    private sealed class EnumerableKeyValuePairConverter<T> : HumanReadableConverter<IEnumerable<KeyValuePair<string, T>>>
-    {
-        protected override void WriteValue(HumanReadableTextWriter writer, IEnumerable<KeyValuePair<string, T>>? value, HumanReadableSerializerOptions options)
-        {
-            Debug.Assert(value is not null);
-            if (options.DictionaryKeyOrder is not null)
-            {
-                value = value.OrderBy(value => value.Key, options.DictionaryKeyOrder);
-            }
-
-            var hasItem = false;
-            foreach (var prop in value)
-            {
-                if (!hasItem)
-                {
-                    writer.StartObject();
-                    hasItem = true;
-                }
-
-                writer.WritePropertyName(prop.Key);
-                HumanReadableSerializer.Serialize(writer, prop.Value, options);
-            }
-
-            if (hasItem)
-            {
-                writer.EndObject();
-            }
-            else
-            {
-                writer.WriteEmptyObject();
-            }
-        }
-    }
 }

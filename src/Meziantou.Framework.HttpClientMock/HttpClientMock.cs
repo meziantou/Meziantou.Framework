45802df25a13f122817c9bc4430142c9c07365ae
Gérald Barré <git@meziantou.net>
2024-11-01 14:51:03 -0400

    HttpClientMock improvements

--- src/Meziantou.Framework.HttpClientMock/HttpClientMock.cs
+++ src/Meziantou.Framework.HttpClientMock/HttpClientMock.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Meziantou.Framework.Internals;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Patterns;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Meziantou.Framework;
 
-public sealed class HttpClientMock : IAsyncDisposable
+public sealed partial class HttpClientMock : IAsyncDisposable
 {
     private bool _running;
 
@@ -26,13 +33,32 @@ public sealed class HttpClientMock : IAsyncDisposable
     }
 
     public HttpClientMock(Action<ILoggingBuilder>? configureLogging)
+        : this(configureLogging, configureServices: null)
+    {
+    }
+
+    public HttpClientMock(Action<ILoggingBuilder>? configureLogging, Action<IServiceCollection>? configureServices)
     {
         var builder = WebApplication.CreateBuilder();
         builder.Services.Configure<ConsoleLifetimeOptions>(opts => opts.SuppressStatusMessages = true);
+        builder.Services.AddSingleton<HttpClient>();
+        builder.Services.AddSingleton<MatcherPolicy, SchemeMatcherPolicy>();
+        builder.Services.AddSingleton<MatcherPolicy, QueryStringMatcherPolicy>();
+        builder.Services.AddSingleton<RequestCounter>();
+        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         builder.Logging.ClearProviders();
         configureLogging?.Invoke(builder.Logging);
+        configureServices?.Invoke(builder.Services);
+
         builder.WebHost.UseTestServer();
         Application = builder.Build();
+        Application.Use(async (context, next) =>
+        {
+            await next().ConfigureAwait(false);
+            var counter = context.RequestServices.GetRequiredService<RequestCounter>();
+            counter.IncrementTotal();
+            counter.IncrementEndpoint(context);
+        });
     }
 
     public WebApplication Application { get; }
@@ -59,6 +85,79 @@ public sealed class HttpClientMock : IAsyncDisposable
     }
 
     public ValueTask DisposeAsync() => Application.DisposeAsync();
+
+    public void ForwardUnknownRequestsToUpstream()
+    {
+        Application.Map(RoutePatternFactory.Parse("{**catchAll}"), () => Results.Extensions.ForwardToUpstream());
+    }
+
+    private IEndpointConventionBuilder Map(string[] methods, string path, Delegate handler)
+    {
+        return MapCore(path, path => Application.MapMethods(path, methods, handler));
+    }
+
+    private IEndpointConventionBuilder Map(string[] methods, string path, RequestDelegate handler)
+    {
+        return MapCore(path, path => Application.MapMethods(path, methods, handler));
+    }
+
+    private static IEndpointConventionBuilder MapCore(string path, Func<string, IEndpointConventionBuilder> mapMethodFunc)
+    {
+        var (scheme, domain, pathOnly, query) = ParseUrl(path);
+        var method = mapMethodFunc(pathOnly);
+        var order = 0;
+
+        if (domain is not null)
+        {
+            method = method.RequireHost(domain);
+        }
+
+        if (scheme is not null)
+        {
+            method = method.WithMetadata(new SchemeMetadata(scheme));
+            order--;
+        }
+
+        if (query is not null)
+        {
+            method = method.WithMetadata(new QueryStringMetadata(query));
+            order--;
+        }
+
+        method.WithOrder(order);
+        return method;
+
+        static (string Scheme, string Domain, string Path, string Query) ParseUrl(string path)
+        {
+            if (path.Contains('#', StringComparison.Ordinal))
+                throw new ArgumentException("Fragment ('#') is not supported", nameof(path));
+
+            if (Uri.TryCreate(path, UriKind.Absolute, out var uri))
+            {
+                var index = uri.PathAndQuery.IndexOf('?', StringComparison.Ordinal);
+                if (index >= 0)
+                {
+                    var pathOnly = uri.PathAndQuery.Substring(0, index);
+                    var query = uri.PathAndQuery.Substring(index);
+                    return (uri.Scheme, uri.Authority, pathOnly, query);
+                }
+
+                return (uri.Scheme, uri.Authority, uri.PathAndQuery, null);
+            }
+            else
+            {
+                var index = path.IndexOf('?', StringComparison.Ordinal);
+                if (index >= 0)
+                {
+                    var pathOnly = path.Substring(0, index);
+                    var query = path.Substring(index);
+                    return (null, null, pathOnly, query);
+                }
+            }
+
+            return (null, null, path, null);
+        }
+    }
 
     private sealed class SingletonLogger(ILogger logger) : ILoggerProvider
     {

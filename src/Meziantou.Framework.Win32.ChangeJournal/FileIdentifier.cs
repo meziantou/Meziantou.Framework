c21f79b6152bd70d23a4e54a6fd63b2a7129cb1b
Gérald Barré <git@meziantou.net>
2024-12-15 22:18:55 -0500

    rename field

--- src/Meziantou.Framework.Win32.ChangeJournal/FileIdentifier.cs
+++ src/Meziantou.Framework.Win32.ChangeJournal/FileIdentifier.cs
@@ -8,19 +8,19 @@ namespace Meziantou.Framework.Win32;
 [StructLayout(LayoutKind.Auto)]
 public readonly struct FileIdentifier : IEquatable<FileIdentifier>
 {
-    private readonly bool _isVersion3;
+    private readonly bool _is128Bits;
     private readonly UInt128 _value;
 
     public FileIdentifier(UInt128 value)
     {
         _value = value;
-        _isVersion3 = true;
+        _is128Bits = true;
     }
 
     public FileIdentifier(ulong value)
     {
         _value = value;
-        _isVersion3 = false;
+        _is128Bits = false;
     }
 
     internal FileIdentifier(FILE_ID_128 parentFileReferenceNumber)
@@ -31,7 +31,7 @@ public readonly struct FileIdentifier : IEquatable<FileIdentifier>
     public bool Equals(FileIdentifier other) => _value == other._value;
     public override bool Equals([NotNullWhen(true)] object? obj) => obj is FileIdentifier identifier && Equals(identifier);
     public override int GetHashCode() => _value.GetHashCode();
-    public override string ToString() => _value.ToString(_isVersion3 ? "x32" : "x16", CultureInfo.InvariantCulture);
+    public override string ToString() => _value.ToString(_is128Bits ? "x32" : "x16", CultureInfo.InvariantCulture);
 
     public static bool operator ==(FileIdentifier left, FileIdentifier right) => left.Equals(right);
     public static bool operator !=(FileIdentifier left, FileIdentifier right) => !(left == right);

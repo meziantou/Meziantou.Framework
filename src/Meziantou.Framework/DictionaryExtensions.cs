6300065f9b81e6f252c03bf000c22af0a2fabf2f
Gérald Barré <git@meziantou.net>
2024-12-25 00:13:42 -0500

    Add Dictionary.GetOrAdd/TryUpdate

---
+++ src/Meziantou.Framework/DictionaryExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Meziantou.Framework;
+public static class DictionaryExtensions
+{
+    public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        where TKey : notnull
+    {
+        ref var dictionaryValue = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out var exists);
+        if (exists)
+            return dictionaryValue;
+
+        dictionaryValue = value;
+        return value;
+    }
+
+    public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, Func<TKey, TValue> valueFactory)
+        where TKey : notnull
+    {
+        ref var dictionaryValue = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out var exists);
+        if (exists)
+            return dictionaryValue;
+
+        dictionaryValue = valueFactory(key);
+        return dictionaryValue;
+    }
+
+    public static bool TryUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        where TKey : notnull
+    {
+        ref var dictionaryValue = ref CollectionsMarshal.GetValueRefOrNullRef(dict, key);
+        if (!Unsafe.IsNullRef(ref dictionaryValue))
+        {
+            dictionaryValue = value;
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool TryUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, Func<TKey, TValue, TValue> valueFactory)
+        where TKey : notnull
+    {
+        ref var dictionaryValue = ref CollectionsMarshal.GetValueRefOrNullRef(dict, key);
+        if (!Unsafe.IsNullRef(ref dictionaryValue))
+        {
+            dictionaryValue = valueFactory(key, dictionaryValue);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ tests/Meziantou.Framework.Tests/DictionaryExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using Xunit;
+
+namespace Meziantou.Framework.Tests;
+public sealed class DictionaryExtensionsTests
+{
+    [Fact]
+    public void GetOrAddTest()
+    {
+        var dict = new Dictionary<string, int>(StringComparer.Ordinal);
+        var value = dict.GetOrAdd("key", 42);
+        Assert.Equal(42, value);
+        Assert.Equal(42, dict["key"]);
+
+        value = dict.GetOrAdd("key", 43);
+        Assert.Equal(42, value);
+        Assert.Equal(42, dict["key"]);
+    }
+
+    [Fact]
+    public void GetOrAddFactoryTest()
+    {
+        var dict = new Dictionary<string, int>(StringComparer.Ordinal);
+        var value = dict.GetOrAdd("key", _ => 42);
+        Assert.Equal(42, value);
+        Assert.Equal(42, dict["key"]);
+
+        value = dict.GetOrAdd("key", _ => 43);
+        Assert.Equal(42, value);
+        Assert.Equal(42, dict["key"]);
+    }
+
+    [Fact]
+    public void TryUpdateTest()
+    {
+        var dict = new Dictionary<string, int>(StringComparer.Ordinal);
+        dict["key"] = 42;
+        var result = dict.TryUpdate("key", 43);
+        Assert.True(result);
+        Assert.Equal(43, dict["key"]);
+        result = dict.TryUpdate("key2", 43);
+        Assert.False(result);
+        Assert.Equal(43, dict["key"]);
+    }
+
+    [Fact]
+    public void TryUpdateFactoryTest()
+    {
+        var dict = new Dictionary<string, int>(StringComparer.Ordinal);
+        dict["key"] = 42;
+        var result = dict.TryUpdate("key", (_, value) => value + 1);
+        Assert.True(result);
+        Assert.Equal(43, dict["key"]);
+
+        result = dict.TryUpdate("key2", (_, value) => value + 1);
+        Assert.False(result);
+        Assert.False(dict.TryGetValue("key2", out _));
+        Assert.Equal(43, dict["key"]);
+    }
+}

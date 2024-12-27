f073a3c05cc51e8344171f04284f65072afc99b8
Gérald Barré <git@meziantou.net>
2024-12-26 21:35:54 -0500

    Add Meziantou.Framework.Http.Hsts

---
+++ src/Meziantou.Framework.Http.Hsts/HstsDomainPolicy.cs
@@ -0,0 +1,25 @@
+﻿namespace Meziantou.Framework.Http;
+
+public sealed class HstsDomainPolicy
+{
+    internal HstsDomainPolicy(string host, DateTimeOffset expiresAt, bool includeSubdomains)
+    {
+        Host = host;
+        ExpiresAt = expiresAt;
+        IncludeSubdomains = includeSubdomains;
+    }
+
+    public string Host { get; }
+    public DateTimeOffset ExpiresAt { get; private set; }
+    public bool IncludeSubdomains { get; private set; }
+
+    public override string ToString()
+    {
+        var result = Host + "; expires=" + ExpiresAt;
+        if (IncludeSubdomains)
+        {
+            result += "; includeSubdomains";
+        }
+        return result;
+    }
+}

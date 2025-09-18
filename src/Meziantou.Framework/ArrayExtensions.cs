6b0afa3e6a2e323b5fdf27fda78ddda57f0ba1c4
Gérald Barré <git@meziantou.net>
2025-09-18 19:53:21 -0400

    Add editorbrowsable

--- src/Meziantou.Framework/ArrayExtensions.cs
+++ src/Meziantou.Framework/ArrayExtensions.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel;
+
 namespace Meziantou.Framework;
 
 public static class ArrayExtensions
 {
     extension<T>(T[] array)
     {
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public int Count => array.Length;
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public long LongCount => array.LongLength;
     }
 }

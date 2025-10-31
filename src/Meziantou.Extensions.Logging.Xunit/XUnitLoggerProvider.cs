385227deb4348239ea19a399bd15db4f0a47f378
Gérald Barré <meziantou@users.noreply.github.com>
2025-10-31 18:53:07 -0400

    Apply suggestion from @meziantou

--- src/Meziantou.Extensions.Logging.Xunit/XUnitLoggerProvider.cs
+++ src/Meziantou.Extensions.Logging.Xunit/XUnitLoggerProvider.cs
@@ -6,19 +6,12 @@ namespace Meziantou.Extensions.Logging.Xunit;
 /// <summary>An <see cref="ILoggerProvider"/> implementation that creates loggers that write to xUnit's <see cref="ITestOutputHelper"/>.</summary>
 /// <example>
 /// <code>
-/// public class MyTests
+/// public class MyTests(ITestOutputHelper output)
 /// {
-///     private readonly ITestOutputHelper _output;
-///
-///     public MyTests(ITestOutputHelper output)
-///     {
-///         _output = output;
-///     }
-///
 ///     [Fact]
 ///     public void MyTest()
 ///     {
-///         using var provider = new XUnitLoggerProvider(_output);
+///         using var provider = new XUnitLoggerProvider(output);
 ///         var loggerFactory = LoggerFactory.Create(builder =>
 ///         {
 ///             builder.Services.AddSingleton{ILoggerProvider}(provider);

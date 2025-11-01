27332b95831251d8bb0996612355d5a23f62392b
Gérald Barré <meziantou@users.noreply.github.com>
2025-10-31 22:00:24 -0400

    Apply suggestion from @meziantou

--- src/Meziantou.Framework/ConcurrentQueueExtensions.cs
+++ src/Meziantou.Framework/ConcurrentQueueExtensions.cs
@@ -5,12 +5,6 @@ namespace Meziantou.Framework;
 /// <summary>
 /// Provides extension methods for <see cref="ConcurrentQueue{T}"/>.
 /// </summary>
-/// <example>
-/// <code>
-/// var queue = new ConcurrentQueue&lt;int&gt;();
-/// queue.EnqueueRange(1, 2, 3, 4, 5);
-/// </code>
-/// </example>
 public static class ConcurrentQueueExtensions
 {
     /// <summary>Enqueues multiple items to the queue.</summary>

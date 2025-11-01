57f4a4afe3c472659ce60c7eb14498e1b0cd7676
Gérald Barré <meziantou@users.noreply.github.com>
2025-10-31 23:54:19 -0400

    Apply suggestion from @meziantou

--- src/Meziantou.Framework.Threading/Threading/Tasks/TaskExtensions.cs
+++ src/Meziantou.Framework.Threading/Threading/Tasks/TaskExtensions.cs
@@ -7,7 +7,7 @@ public static partial class TaskExtensions
     /// <param name="task">The task to forget.</param>
     /// <remarks>
     /// This method ensures that exceptions in the task are observed to prevent unobserved task exceptions.
-    /// See https://www.meziantou.net/fire-and-forget-a-task-in-dotnet.htm for more information.
+    /// See <see href="https://www.meziantou.net/fire-and-forget-a-task-in-dotnet.htm" /> for more information.
     /// </remarks>
     public static void Forget(this Task task)
     {

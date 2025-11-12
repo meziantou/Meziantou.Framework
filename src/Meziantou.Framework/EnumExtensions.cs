4ab4db58f3bf591130933d45508029c7d5e77652
Gérald Barré <git@meziantou.net>
2025-11-12 10:54:33 -0500

    Remove extra overloads and add XML comments

--- src/Meziantou.Framework/EnumExtensions.cs
+++ src/Meziantou.Framework/EnumExtensions.cs
@@ -3,24 +3,55 @@ namespace Meziantou.Framework;
 
 public static class EnumExtensions
 {
-    extension<T>(T) where T : struct, Enum
+    extension<TEnum>(TEnum) where TEnum : struct, Enum
     {
-        public static T Parse(string value)
-            => Enum.Parse<T>(value);
+        /// <summary>
+        /// Converts the string representation of the name or numeric value of one or more enumerated constants specified by <typeparamref name="TEnum"/> to an equivalent enumerated object.
+        /// A parameter specifies whether the operation is case-insensitive.
+        /// </summary>
+        /// <param name="value">A string containing the name or value to convert.</param>
+        /// <param name="ignoreCase"><see langword="true"/> to ignore case; <see langword="false"/> to regard case.</param>
+        /// <returns><typeparamref name="TEnum"/> An object of type <typeparamref name="TEnum"/> whose value is represented by <paramref name="value"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><typeparamref name="TEnum"/> is not an <see cref="Enum"/> type</exception>
+        /// <exception cref="ArgumentException"><paramref name="value"/> does not contain enumeration information</exception>
+        public static TEnum Parse(string value, bool ignoreCase)
+            => Enum.Parse<TEnum>(value, ignoreCase);
 
-        public static T Parse(string value, bool ignoreCase)
-            => Enum.Parse<T>(value, ignoreCase);
+        /// <summary>
+        /// Converts the span of chars representation of the name or numeric value of one or more enumerated constants specified by <typeparamref name="TEnum"/> to an equivalent enumerated object.
+        /// A parameter specifies whether the operation is case-insensitive.
+        /// </summary>
+        /// <param name="value">A span containing the name or value to convert.</param>
+        /// <param name="ignoreCase"><see langword="true"/> to ignore case; <see langword="false"/> to regard case.</param>
+        /// <returns><typeparamref name="TEnum"/> An object of type <typeparamref name="TEnum"/> whose value is represented by <paramref name="value"/>.</returns>
+        /// <exception cref="ArgumentException"><typeparamref name="TEnum"/> is not an <see cref="Enum"/> type</exception>
+        /// <exception cref="ArgumentException"><paramref name="value"/> does not contain enumeration information</exception>
+        public static TEnum Parse(ReadOnlySpan<char> value, bool ignoreCase)
+            => Enum.Parse<TEnum>(value, ignoreCase);
 
-        public static T Parse(ReadOnlySpan<char> value)
-            => Enum.Parse<T>(value);
-
-        public static T Parse(ReadOnlySpan<char> value, bool ignoreCase)
-            => Enum.Parse<T>(value, ignoreCase);
-
-        public static bool TryParse([NotNullWhen(true)] string? value, bool ignoreCase, out T result)
+        /// <summary>
+        /// Converts the string representation of the name or numeric value of one or more enumerated constants to an equivalent enumerated object.
+        /// A parameter specifies whether the operation is case-sensitive.
+        /// </summary>
+        /// <param name="value">The string representation of the name or numeric value of one or more enumerated constants.</param>
+        /// <param name="ignoreCase"><see langword="true"/> to ignore case; <see langword="false"/> to consider case.</param>
+        /// <param name="result">When this method returns <see langword="true"/>, an object containing an enumeration constant representing the parsed value.</param>
+        /// <returns><see langword="true"/> if the conversion succeeded; <see langword="false"/> otherwise.</returns>
+        /// <exception cref="ArgumentException"><typeparamref name="TEnum"/> is not an enumeration type</exception>
+        public static bool TryParse([NotNullWhen(true)] string? value, bool ignoreCase, out TEnum result)
             => Enum.TryParse(value, ignoreCase, out result);
 
-        public static bool TryParse(ReadOnlySpan<char> value, bool ignoreCase, out T result)
+        /// <summary>
+        /// Converts the span of chars representation of the name or numeric value of one or more enumerated constants to an equivalent enumerated object.
+        /// A parameter specifies whether the operation is case-sensitive.
+        /// </summary>
+        /// <param name="value">The span representation of the name or numeric value of one or more enumerated constants.</param>
+        /// <param name="ignoreCase"><see langword="true"/> to ignore case; <see langword="false"/> to consider case.</param>
+        /// <param name="result">When this method returns <see langword="true"/>, an object containing an enumeration constant representing the parsed value.</param>
+        /// <returns><see langword="true"/> if the conversion succeeded; <see langword="false"/> otherwise.</returns>
+        /// <exception cref="ArgumentException"><typeparamref name="TEnum"/> is not an enumeration type</exception>
+        public static bool TryParse(ReadOnlySpan<char> value, bool ignoreCase, out TEnum result)
             => Enum.TryParse(value, ignoreCase, out result);
     }
 }

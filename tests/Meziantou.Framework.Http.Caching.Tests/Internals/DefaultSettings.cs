d9985aa635ae66031710adf8ffebb15d54a9ccb1
Gérald Barré <git@meziantou.net>
2026-01-02 23:14:16 -0500

    Update settings

---
+++ tests/Meziantou.Framework.Http.Caching.Tests/Internals/DefaultSettings.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+using Meziantou.Framework.InlineSnapshotTesting;
+
+namespace Meziantou.Framework.Http.Caching.Tests.Internals;
+
+internal static class DefaultSettings
+{
+    [ModuleInitializer]
+    public static void ConfigureSnapshot()
+    {
+        InlineSnapshotSettings.Default = InlineSnapshotSettings.Default with
+        {
+            SnapshotUpdateStrategy = SnapshotUpdateStrategy.MergeTool,
+        };
+    }
+}

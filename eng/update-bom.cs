5735f4d862cd6b27d2f631a1806a72fae14b2f1e
Gérald Barré <git@meziantou.net>
2026-03-24 12:13:24 -0400

    Improve performance

--- eng/update-bom.cs
+++ eng/update-bom.cs
@@ -1,7 +1,9 @@
 #:project ../src/Meziantou.Framework.FullPath/Meziantou.Framework.FullPath.csproj
 using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using Meziantou.Framework;
 
 if (args.Length > 0 && args[0] is "--help" or "-h")
@@ -17,25 +19,24 @@ var srcRootPath = Path.Combine(rootPath, "src");
 var editedFiles = 0;
 
 var extensions = new[] { "*.cs", "*.csproj", "*.fsproj", "*.proj", "*.props", "*.targets", "*.save", "*.slnx", "*.ps1", "*.yml", "*.yaml", "*.md", "*.json" };
-foreach (var ext in extensions)
-{
-    foreach (var file in Directory.EnumerateFiles(srcRootPath, ext, SearchOption.AllDirectories))
-    {
-        Console.WriteLine($"Processing {file}");
-        var content = File.ReadAllBytes(file);
-        if (content.Length < 3)
-        {
-            continue;
-        }
+var files = extensions.SelectMany(ext => Directory.EnumerateFiles(srcRootPath, ext, SearchOption.AllDirectories));
 
-        if (content[0] == 0xEF && content[1] == 0xBB && content[2] == 0xBF)
-        {
-            Console.WriteLine($"WARNING: File {file} contains BOM. Removing it.");
-            File.WriteAllBytes(file, content.AsSpan(3).ToArray());
-            editedFiles++;
-        }
+Parallel.ForEach(files, file =>
+{
+    Console.WriteLine($"Processing {file}");
+    var content = File.ReadAllBytes(file);
+    if (content.Length < 3)
+    {
+        return;
     }
-}
+
+    if (content[0] == 0xEF && content[1] == 0xBB && content[2] == 0xBF)
+    {
+        Console.WriteLine($"WARNING: File {file} contains BOM. Removing it.");
+        File.WriteAllBytes(file, content.AsSpan(3).ToArray());
+        Interlocked.Increment(ref editedFiles);
+    }
+});
 
 return editedFiles;
 

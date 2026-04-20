d39d0ff8fbd8674c3305c286042348a5813c1592
Gérald Barré <git@meziantou.net>
2026-04-20 19:54:55 -0400

    wip

--- tests/Meziantou.Framework.InlineSnapshotTesting.Tests/InlineSnapshotTests.cs
+++ tests/Meziantou.Framework.InlineSnapshotTesting.Tests/InlineSnapshotTests.cs
@@ -1145,13 +1145,13 @@ public sealed class InlineSnapshotTests(ITestOutputHelper testOutputHelper)
         Assert.NotNull(dotnetPath);
 
         testOutputHelper.WriteLine("Restoring project");
-        await ExecuteDotNet("restore", expectedExitCode: 0);
+        await ExecuteDotNet("restore --disable-build-servers", expectedExitCode: 0);
 
         testOutputHelper.WriteLine("Building project");
-        await ExecuteDotNet("build", expectedExitCode: 0);
+        await ExecuteDotNet("build --no-restore --disable-build-servers", expectedExitCode: 0);
 
         testOutputHelper.WriteLine("Running project");
-        await ExecuteDotNet("run");
+        await ExecuteDotNet("run --no-build --disable-build-servers");
 
         var actual = File.ReadAllText(mainPath);
         expected ??= source;

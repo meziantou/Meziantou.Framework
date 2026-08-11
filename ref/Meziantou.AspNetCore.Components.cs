82769c09b27d7b4758377db3eee066ff04ac09a2
Gérald Barré <git@meziantou.net>
2026-08-11 17:03:15 -0400

    wip

--- ref/Meziantou.AspNetCore.Components.cs
+++ ref/Meziantou.AspNetCore.Components.cs
@@ -186,13 +186,3 @@ namespace Meziantou.AspNetCore.Components
         protected void Execute() { }
     }
 }
-namespace Meziantou.AspNetCore.Components.Internals
-{
-    public static class JsRuntimeExtensions
-    {
-        public static System.Threading.Tasks.ValueTask SafeInvokeVoidAsync(this Microsoft.JSInterop.IJSRuntime jsRuntime, string identifier, params object?[] args) => throw null;
-        public static System.Threading.Tasks.ValueTask SafeInvokeVoidAsync(this Microsoft.JSInterop.IJSRuntime jsRuntime, string identifier, System.Threading.CancellationToken cancellationToken, params object?[] args) => throw null;
-        public static System.Threading.Tasks.ValueTask SafeInvokeVoidAsync(this Microsoft.JSInterop.IJSObjectReference jsRuntime, string identifier, System.Threading.CancellationToken cancellationToken, params object?[] args) => throw null;
-        public static System.Threading.Tasks.ValueTask SafeInvokeVoidAsync(this Microsoft.JSInterop.IJSObjectReference jsRuntime, string identifier, params object?[] args) => throw null;
-    }
-}

23989bb95bbc12b356bb2896581f1e9b550aa65b
Gérald Barré <git@meziantou.net>
2026-08-11 16:52:19 -0400

    wip

--- src/Meziantou.AspNetCore.Components/Internals/JsRuntimeExtensions.cs
+++ src/Meziantou.AspNetCore.Components/Internals/JsRuntimeExtensions.cs
@@ -15,6 +15,28 @@ public static class JsRuntimeExtensions
         }
     }
 
+    public static async ValueTask SafeInvokeVoidAsync(this IJSRuntime jsRuntime, string identifier, CancellationToken cancellationToken, params object?[] args)
+    {
+        try
+        {
+            await jsRuntime.InvokeVoidAsync(identifier, cancellationToken, args);
+        }
+        catch
+        {
+        }
+    }
+
+    public static async ValueTask SafeInvokeVoidAsync(this IJSObjectReference jsRuntime, string identifier, CancellationToken cancellationToken, params object?[] args)
+    {
+        try
+        {
+            await jsRuntime.InvokeVoidAsync(identifier, cancellationToken, args);
+        }
+        catch
+        {
+        }
+    }
+
     public static async ValueTask SafeInvokeVoidAsync(this IJSObjectReference jsRuntime, string identifier, params object?[] args)
     {
         try

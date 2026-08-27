a57bdec7ac5bc54f7a30712bb7f97471328f9dc0
Gérald Barré <git@meziantou.net>
2026-08-27 01:28:23 -0400

    Implement IComparable on Usn

--- ref/Meziantou.Framework.Win32.ChangeJournal.cs
+++ ref/Meziantou.Framework.Win32.ChangeJournal.cs
@@ -140,7 +140,7 @@ namespace Meziantou.Framework.Win32
         ClientReplicationManagement = 8
     }
 
-    public readonly struct Usn : System.IEquatable<Meziantou.Framework.Win32.Usn>
+    public readonly struct Usn : System.IComparable, System.IComparable<Meziantou.Framework.Win32.Usn>, System.IEquatable<Meziantou.Framework.Win32.Usn>
     {
         public static Meziantou.Framework.Win32.Usn Zero { get => throw null; }
         public long Value { get => throw null; }
@@ -149,6 +149,8 @@ namespace Meziantou.Framework.Win32
         public override bool Equals([System.Diagnostics.CodeAnalysis.NotNullWhen(true)] object? obj) => throw null;
         public bool Equals(Meziantou.Framework.Win32.Usn other) => throw null;
         public override int GetHashCode() => throw null;
+        public int CompareTo(Meziantou.Framework.Win32.Usn other) => throw null;
+        public int CompareTo(object? obj) => throw null;
         public static bool operator ==(Meziantou.Framework.Win32.Usn usn1, Meziantou.Framework.Win32.Usn usn2) => throw null;
         public static bool operator !=(Meziantou.Framework.Win32.Usn usn1, Meziantou.Framework.Win32.Usn usn2) => throw null;
         public static bool operator <=(Meziantou.Framework.Win32.Usn usn1, Meziantou.Framework.Win32.Usn usn2) => throw null;

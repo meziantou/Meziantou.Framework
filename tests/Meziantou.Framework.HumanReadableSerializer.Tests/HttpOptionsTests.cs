f233da49178eb64feb3006ca6f085361ca1ab549
Gérald Barré <git@meziantou.net>
2026-08-29 23:20:49 -0400

    Apply OmitProtocolVersion to the request message instead of the response message

--- tests/Meziantou.Framework.HumanReadableSerializer.Tests/HttpOptionsTests.cs
+++ tests/Meziantou.Framework.HumanReadableSerializer.Tests/HttpOptionsTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Meziantou.Framework.HumanReadable.Converters;
 
 namespace Meziantou.Framework.HumanReadable.Tests;
@@ -8,6 +9,13 @@ public sealed class HttpOptionsTests : SerializerTestsBase
     {
         var serializerOptions = new HumanReadableSerializerOptions()
             .AddHttpConverters(new HumanReadableHttpOptions { ResponseMessageOptions = options });
+
+        AssertSerialization(value, serializerOptions, expected);
+    }
+
+    private static void AssertSerialization(object value, HumanReadableHttpOptions options, string expected)
+    {
+        var serializerOptions = new HumanReadableSerializerOptions().AddHttpConverters(options);
 
         AssertSerialization(value, serializerOptions, expected);
     }
@@ -34,11 +42,101 @@ public sealed class HttpOptionsTests : SerializerTestsBase
             RequestMessage:
               Method: GET
               RequestUri: http://example.com/foo
+              Content: <null>
+            """);
+    }
+
+    [Fact]
+    public void RequestMessage_Full_KeepsNonDefaultProtocolVersion()
+    {
+        using var httpContent = new HttpResponseMessage()
+        {
+            Headers =
+            {
+                Date = DateTimeOffset.UtcNow,
+            },
+            Content = new StringContent("test"),
+            RequestMessage = new HttpRequestMessage(HttpMethod.Get, "http://example.com/foo")
+            {
+                Version = HttpVersion.Version20,
+                VersionPolicy = HttpVersionPolicy.RequestVersionExact,
+            },
+        };
+
+        AssertSerialization(httpContent, new HumanReadableHttpResponseMessageOptions { RequestMessageFormat = HttpRequestMessageFormat.Full }, """
+            StatusCode: 200 (OK)
+            Content:
+              Headers:
+                Content-Type: text/plain; charset=utf-8
+              Value: test
+            RequestMessage:
+              Method: GET
+              RequestUri: http://example.com/foo
+              Version: 2.0
+              VersionPolicy: RequestVersionExact
+              Content: <null>
+            """);
+    }
+
+    [Fact]
+    public void RequestMessage_Full_ProtocolVersionIsKeptWhenNotOmitted()
+    {
+        using var httpContent = new HttpResponseMessage()
+        {
+            Headers =
+            {
+                Date = DateTimeOffset.UtcNow,
+            },
+            Content = new StringContent("test"),
+            RequestMessage = new HttpRequestMessage(HttpMethod.Get, "http://example.com/foo"),
+        };
+
+        var options = new HumanReadableHttpOptions
+        {
+            RequestMessageOptions = new HumanReadableHttpRequestMessageOptions { OmitProtocolVersion = false },
+            ResponseMessageOptions = new HumanReadableHttpResponseMessageOptions { RequestMessageFormat = HttpRequestMessageFormat.Full },
+        };
+
+        AssertSerialization(httpContent, options, """
+            StatusCode: 200 (OK)
+            Content:
+              Headers:
+                Content-Type: text/plain; charset=utf-8
+              Value: test
+            RequestMessage:
+              Method: GET
+              RequestUri: http://example.com/foo
               Version: 1.1
               VersionPolicy: RequestVersionOrLower
               Content: <null>
             """);
     }
+
+    [Fact]
+    public void ResponseMessage_ProtocolVersionIsNotHiddenByTheRequestOptions()
+    {
+        using var httpContent = new HttpResponseMessage()
+        {
+            Version = HttpVersion.Version20,
+            Content = new StringContent("test"),
+        };
+
+        var options = new HumanReadableHttpOptions
+        {
+            RequestMessageOptions = new HumanReadableHttpRequestMessageOptions { OmitProtocolVersion = true },
+            ResponseMessageOptions = new HumanReadableHttpResponseMessageOptions { OmitProtocolVersion = false },
+        };
+
+        AssertSerialization(httpContent, options, """
+            StatusCode: 200 (OK)
+            Version: 2.0
+            Content:
+              Headers:
+                Content-Type: text/plain; charset=utf-8
+              Value: test
+            """);
+    }
+
     [Fact]
     public void RequestMessage_Uri()
     {

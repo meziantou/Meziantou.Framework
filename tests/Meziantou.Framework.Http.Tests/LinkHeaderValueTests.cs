736dd6a6f6e038fe3ea7c23a8b650b5d63321ac1
Gérald Barré <meziantou@users.noreply.github.com>
2026-08-31 17:59:49 -0700

    Treat CR and LF as whitespace when parsing Link headers (#2760)

--- tests/Meziantou.Framework.Http.Tests/LinkHeaderValueTests.cs
+++ tests/Meziantou.Framework.Http.Tests/LinkHeaderValueTests.cs
@@ -136,6 +136,25 @@ public sealed class LinkHeaderValueTests
         Assert.Null(links.GetLink("prev"));
     }
 
+    [Theory]
+    [InlineData("<a>;\r\n rel=next")]
+    [InlineData("<a>;\r\nrel=next")]
+    [InlineData("<a>;\n rel=next")]
+    [InlineData("<a>;\r\n\trel=next")]
+    public void Parse_TreatsCarriageReturnAndLineFeedAsWhiteSpace(string header)
+    {
+        var link = Assert.Single(LinkHeaderValue.Parse(header));
+        Assert.Equal("a", link.Url);
+        Assert.Equal("next", link.Rel);
+    }
+
+    [Fact]
+    public void Parse_TerminatesAValuelessParameterNameAtALineBreak()
+    {
+        var link = Assert.Single(LinkHeaderValue.Parse("<a>; abc\r\n; def"));
+        Assert.Equal(["abc", "def"], link.Parameters.Select(p => p.Key));
+    }
+
     private sealed class CustomHttpHeaders : HttpHeaders
     {
     }
